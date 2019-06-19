96ac09bf9c85a095af585d9b0db99bb95ab98b9a
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-06-19 10:48:46 -0300

    added LOESS correction

--- ms_feature_validation/filter.py
+++ ms_feature_validation/filter.py
@@ -1,10 +1,38 @@
 import pandas as pd
+from scipy.interpolate import CubicSpline
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 
 def blank_correction(data, blanks, mode, blank_relation):
-    corrector = {"max": lambda x: x.max(), "mean": lambda x: x.mean()}
+    """
+    Correct data using blanks.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        Data to correct.
+    blanks : pandas.DataFrame
+        Blank Samples.
+    mode : {'mean', 'max'}, optional
+    blank_relation: int.
+        Number of times to substract the correction.
+
+    Returns
+    -------
+    corrected : pandas.DataFrame
+        Data with applied correction
+
+    Notes
+    -----
+
+    Blank correction is applied for each feature in the following way:
+
+    .. math:: X_{corrected} = X_{uncorrected} - blank_relation * mode(X_{blank})
+    """
+    corrector = {"max": lambda x: x.max(axis=0),
+                 "mean": lambda x: x.mean(axis=0)}
     correction = corrector[mode](blanks) * blank_relation
-    corrected = data.subtract(correction)
+    corrected = data - correction
     corrected[corrected < 0] = 0
     return corrected
 
@@ -17,9 +45,9 @@ def prevalence_filter(data, lb, ub):
     ----------
     data : pandas.DataFrame
     lb : float.
-         Relative lower bound of prevalence.
+        Relative lower bound of prevalence.
     ub : float.
-         Relative upper bound of prevalence.
+        Relative upper bound of prevalence.
     Returns
     -------
     outside_bounds_columns: pandas.Index
@@ -34,15 +62,16 @@ def prevalence_filter(data, lb, ub):
 def variation_filter(data, lb, ub, robust):
     """
     Return columns with variation outside the [lb, ub] interval.
+
     Parameters
     ----------
     data : pandas.DataFrame
     lb : float
-         lower bound of variation
+        lower bound of variation
     ub : float
-         upper bound of variation
+        upper bound of variation
     robust : bool
-             if True uses iqr as a metric of variation. if False uses cv
+        if True uses iqr as a metric of variation. if False uses cv
     Returns
     -------
     remove_features : pandas.Index
@@ -61,3 +90,39 @@ def iqr(df):
 
 def cv(df):
     return df.std() / df.mean()
+
+
+def loess_correction(corrector_order, corrector, samples_order, samples):
+    """
+    Correct instrument response drift using LOESS regression [1].
+
+    Parameters
+    ----------
+    corrector_order : Iterable[int]
+        Order of corrector samples.
+    corrector: pandas.DataFrame
+    samples_order: Iterable[int]
+        Order of samples
+    samples: pandas.DataFrame
+
+    Returns
+    -------
+    corrected: pandas.DataFrame
+
+    References.
+    -----
+    .. [1] W B Dunn *et al*, "Procedures for large-scale metabolic profiling of
+    serum and plasma using gas chromatography and liquid chromatography coupled
+    to mass spectrometry", Nature Protocols volume 6, pages 1060–1083 (2011).
+
+    """
+    corrected = pd.DataFrame()
+    for features in samples.columns:
+        corrector_loess = lowess(corrector[features], corrector_order,
+                                 return_sorted=False)
+        spline = CubicSpline(corrector_order,
+                             corrector_loess / corrector[features])
+        correction_factor = spline(samples_order)
+        corrected[features] = correction_factor * samples[features]
+    return corrected
+
