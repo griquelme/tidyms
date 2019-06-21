02a10a0670d88151c20ef5b09178bb75392a25ae
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-06-21 17:38:53 -0300

    minor changes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import ms_feature_validation
+import ms_feature_validation as mfv
 
 if __name__ == "__main__":
-    data = ms_feature_validation.process.data_container_from_excel("examples/example_sim.xlsx")
-    config = ms_feature_validation.process.read_config("examples/config.yaml")
+    data = mfv.process.data_container_from_excel("examples/example_sim.xlsx")
+    config = mfv.process.read_config("examples/config.yaml")
     pipeline = ms_feature_validation.process.pipeline_from_list(config["Pipeline"])
     pipeline.transform(data)
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 setup(name='ms_feature_validation',
       packages=['ms_feature_validation'],
-      install_requires=['pandas>=0.24.2', 'pyopenms>=2.4.0', 'numpy>=1.16.4',
+      install_requires=['pandas>=0.24', 'pyopenms>=2.4', 'numpy>=1.16',
                         'pyyaml'])
