0e095bd8022e25ef3ea3a74c6eb0a0f89c286d44
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-06-28 10:26:00 -0300

    added chromatogram module

--- ms_feature_validation/__init__.py
+++ ms_feature_validation/__init__.py
@@ -1,3 +1,4 @@
 from . import fileio
 from . import filter
 from . import process
+from . import chromatograms
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 setup(name='ms_feature_validation',
       packages=['ms_feature_validation'],
-      install_requires=['pandas>=0.24', 'pyopenms>=2.4', 'numpy>=1.16',
-                        'pyyaml'])
+      install_requires=['pandas>=0.24', 'pyopenms>=2.4', 'numpy>=1.15',
+                        'pyyaml', 'statsmodels', 'scipy'])
