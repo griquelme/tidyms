681d9d4d1ae523f93c870e4916cdece352a8906a
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-07-03 14:13:03 -0300

    modified blank correction, prevalence filter and variation filter

--- ms_feature_validation/filter.py
+++ ms_feature_validation/filter.py
@@ -1,18 +1,23 @@
+import numpy as np
 import pandas as pd
 from scipy.interpolate import CubicSpline
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 
-def blank_correction(data, blanks, mode, blank_relation):
+def blank_correction(data, classes, blank_classes, sample_classes, mode, blank_relation):
     """
-    Correct data using blanks.
+    Correct samples using blanks.
 
     Parameters
     ----------
     data : pandas.DataFrame
         Data to correct.
-    blanks : pandas.DataFrame
-        Blank Samples.
+    classes : pandas.Series
+        Samples class labels.
+    blank_classes : list[str]
+        Classes to be used as blanks.
+    sample_classes : list[str]
+        Classes to be used as samples
     mode : {'mean', 'max'}, optional
     blank_relation: int.
         Number of times to substract the correction.
@@ -31,45 +36,69 @@ def blank_correction(data, blanks, mode, blank_relation):
     """
     corrector = {"max": lambda x: x.max(axis=0),
                  "mean": lambda x: x.mean(axis=0)}
+    samples = data[classes.isin(sample_classes)]
+    blanks = data[classes.isin(blank_classes)]
     correction = corrector[mode](blanks) * blank_relation
-    corrected = data - correction
+    corrected = samples - correction
     corrected[corrected < 0] = 0
-    return corrected
+    data[classes.isin(sample_classes)] = corrected
+    return data
 
 
-def prevalence_filter(data, lb, ub):
+def prevalence_filter(data, classes, include_classes, lb, ub, intraclass):
     """
     Return columns with relative counts outside the [lb, ub] interval.
 
     Parameters
     ----------
     data : pandas.DataFrame
+    classes : pandas.Series
+        sample class labels
+    include_classes : list[str]
+        classes included in the filter
     lb : float.
         Relative lower bound of prevalence.
     ub : float.
         Relative upper bound of prevalence.
+    intraclass : bool
+        if True computes prevalence for each class separately and return columns
+        that are outside bounds in all of `include_classes`.
     Returns
     -------
     outside_bounds_columns: pandas.Index
     """
-    nlb, nub = lb * data.shape[0], ub * data.shape[0]
-    column_counts = (data > 0).sum()
-    bounds = (column_counts <= nlb) | (column_counts >= nub)
-    outside_bounds_columns = column_counts[bounds].index
+    # selects include_classes
+    classes = classes[classes.isin(include_classes)]
+    data = data.loc[classes.index, :]
+    # pipeline for prevalence filter
+    is_outside_bounds = ((data > 0)
+                         .pipe(grouper(classes, intraclass))
+                         .sum()
+                         .pipe(normalizer(classes, intraclass))
+                         .pipe(bounds_checker(lb, ub, intraclass)))
+    outside_bounds_columns = is_outside_bounds[is_outside_bounds].index
     return outside_bounds_columns
 
 
-def variation_filter(data, lb, ub, robust):
+def variation_filter(data, classes, include_classes, lb, ub,
+                     intraclass, robust):
     """
     Return columns with variation outside the [lb, ub] interval.
 
     Parameters
     ----------
     data : pandas.DataFrame
+    classes : pandas.Series
+        class labels of samples.
+    include_classes : list[str]
+        classes included in the filter
     lb : float
         lower bound of variation
     ub : float
         upper bound of variation
+    intraclass : bool
+         if True computes prevalence for each class separately and return columns
+        that are outside bounds in all of `include_classes`.
     robust : bool
         if True uses iqr as a metric of variation. if False uses cv
     Returns
@@ -77,19 +106,40 @@ def variation_filter(data, lb, ub, robust):
     remove_features : pandas.Index
     """
 
-    variation = iqr if robust else cv
-    data_variation = variation(data)
-    bounds = (data_variation <= lb) | (data_variation >= ub)
-    outside_bounds_columns = data_variation[bounds].index
+    # selects include_classes
+    classes = classes[classes.isin(include_classes)]
+    data = data.loc[classes.index, :]
+    # pipeline for variation filter
+    is_outside_bound = (data.pipe(grouper(classes, intraclass))
+                        .pipe(variation(robust))
+                        .pipe(bounds_checker(lb, ub, intraclass)))
+    outside_bounds_columns = is_outside_bound[is_outside_bound].index
     return outside_bounds_columns
 
 
-def iqr(df):
-    return (df.quantile(0.75) - df.quantile(0.25)) / df.quantile(0.5)
+
+def normalizer(classes, intraclass):
+    class_counts = classes.value_counts()
+    n =class_counts.sum()
+    return lambda x: x.divide(class_counts, axis=0) if intraclass else x / n
 
 
-def cv(df):
-    return df.std() / df.mean()
+def grouper(classes, intraclass):
+    return lambda x: x.groupby(classes) if intraclass else x
+
+def bounds_checker(lb, ub, intraclass):
+    if intraclass:
+        return lambda x: ((x < lb) | (x > ub)).any()
+    else:
+        return lambda x: ((x < lb) | (x > ub))
+
+
+def variation(robust):
+    if robust:
+        func = lambda x: (x.quantile(0.75) - x.quantile(0.25)) / x.quantile(0.5)
+    else:
+        func = lambda x: x.std() / x.mean()
+    return func
 
 
 def cspline_correction(x, y, xq):
