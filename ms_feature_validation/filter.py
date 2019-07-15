6951e3c6663b3dc57f5a7670b87bc6c1f904483f
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-07-14 22:18:54 -0300

    Changed argument names in filter functions.

--- ms_feature_validation/filter.py
+++ ms_feature_validation/filter.py
@@ -8,7 +8,8 @@ from scipy.interpolate import CubicSpline
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 
-def blank_correction(data, classes, blank_classes, sample_classes, mode, blank_relation):
+def blank_correction(data, classes, corrector_classes, process_classes,
+                     mode, blank_relation):
     """
     Correct samples using blanks.
 
@@ -18,9 +19,9 @@ def blank_correction(data, classes, blank_classes, sample_classes, mode, blank_r
         Data to correct.
     classes : pandas.Series
         Samples class labels.
-    blank_classes : list[str]
+    corrector_classes : list[str]
         Classes to be used as blanks.
-    sample_classes : list[str]
+    process_classes : list[str]
         Classes to be used as samples
     mode : {'mean', 'max'}, optional
     blank_relation: int.
@@ -40,16 +41,16 @@ def blank_correction(data, classes, blank_classes, sample_classes, mode, blank_r
     """
     corrector = {"max": lambda x: x.max(axis=0),
                  "mean": lambda x: x.mean(axis=0)}
-    samples = data[classes.isin(sample_classes)]
-    blanks = data[classes.isin(blank_classes)]
+    samples = data[classes.isin(process_classes)]
+    blanks = data[classes.isin(corrector_classes)]
     correction = corrector[mode](blanks) * blank_relation
     corrected = samples - correction
     corrected[corrected < 0] = 0
-    data[classes.isin(sample_classes)] = corrected
+    data[classes.isin(process_classes)] = corrected
     return data
 
 
-def prevalence_filter(data, classes, include, lb, ub, intraclass):
+def prevalence_filter(data, classes, process_classes, lb, ub, intraclass):
     """
     Return columns with relative counts outside the [lb, ub] interval.
 
@@ -58,7 +59,7 @@ def prevalence_filter(data, classes, include, lb, ub, intraclass):
     data : pandas.DataFrame
     classes : pandas.Series
         sample class labels
-    include : list[str]
+    process_classes : list[str]
         classes included in the filter
     lb : float.
         Relative lower bound of prevalence.
@@ -71,8 +72,8 @@ def prevalence_filter(data, classes, include, lb, ub, intraclass):
     -------
     outside_bounds_columns: pandas.Index
     """
-    # selects include
-    classes = classes[classes.isin(include)]
+    # selects process_classes
+    classes = classes[classes.isin(process_classes)]
     data = data.loc[classes.index, :]
     # pipeline for prevalence filter
     is_outside_bounds = ((data > 0)
@@ -84,7 +85,7 @@ def prevalence_filter(data, classes, include, lb, ub, intraclass):
     return outside_bounds_columns
 
 
-def variation_filter(data, classes, include, lb, ub,
+def variation_filter(data, classes, process_classes, lb, ub,
                      intraclass, robust):
     """
     Return columns with variation outside the [lb, ub] interval.
@@ -94,7 +95,7 @@ def variation_filter(data, classes, include, lb, ub,
     data : pandas.DataFrame
     classes : pandas.Series
         class labels of samples.
-    include : list[str]
+    process_classes : list[str]
         classes included in the filter
     lb : float
         lower bound of variation
@@ -109,8 +110,8 @@ def variation_filter(data, classes, include, lb, ub,
     -------
     remove_features : pandas.Index
     """
-    # selects include
-    classes = classes[classes.isin(include)]
+    # selects process_classes
+    classes = classes[classes.isin(process_classes)]
     data = data.loc[classes.index, :]
     # pipeline for variation filter
     is_outside_bound = (data.pipe(grouper(classes, intraclass))
@@ -166,8 +167,8 @@ def loess_correction(x, y, xq, yq, **kwargs):
     return y_corrected
 
 
-def batch_correction(data, run_order, classes, corrector_class, sample_classes,
-                     mode, **kwargs):
+def batch_correction(data, run_order, classes, corrector_classes,
+                     process_classes, mode, **kwargs):
     """
     Correct instrument response drift using LOESS regression [1].
 
@@ -178,9 +179,9 @@ def batch_correction(data, run_order, classes, corrector_class, sample_classes,
         run order of samples
     classes : pandas.Series
         class label for samples
-    corrector_class : str
+    corrector_classes : str
         label of corrector class
-    sample_classes: list[str]
+    process_classes: list[str]
         samples to correct
     mode: {'loess', 'splines'}
     kwargs: optional arguments to pass to loess corrector.
@@ -198,9 +199,9 @@ def batch_correction(data, run_order, classes, corrector_class, sample_classes,
     """
     corrector = {"loess": loess_correction, "splines": cspline_correction}
     corrector = corrector[mode]
-    corrector_class_mask = classes.isin([corrector_class])
+    corrector_class_mask = classes.isin([corrector_classes])
     corrector_run = run_order[corrector_class_mask]
-    sample_classes_mask = classes.isin(sample_classes)
+    sample_classes_mask = classes.isin(process_classes)
     sample_run = run_order[sample_classes_mask]
     corrected = data.apply(lambda x: corrector(corrector_run,
                                                x[corrector_class_mask],
@@ -210,8 +211,8 @@ def batch_correction(data, run_order, classes, corrector_class, sample_classes,
     return corrected
 
 
-def interbatch_correction(data, batch, run_order, classes, corrector_class,
-                          sample_classes, mode, **kwargs):
+def interbatch_correction(data, batch, run_order, classes, corrector_classes,
+                          process_classes, mode, **kwargs):
     """
     Apply batch correction to several batches. Interbatch correction is achieved
     using a scaling factor obtained from the mean values in corrector samples.
@@ -224,9 +225,9 @@ def interbatch_correction(data, batch, run_order, classes, corrector_class,
     run_order : pd.Series
     classes : pd.Series
         Class labels for samples
-    corrector_class : str
+    corrector_classes : str
         class used to correct samples
-    sample_classes : list[str]
+    process_classes : list[str]
         class labels to correct.
     mode : {"loess", "splines"}
     kwargs : optional arguments to pass to loess corrector
@@ -235,12 +236,12 @@ def interbatch_correction(data, batch, run_order, classes, corrector_class,
     -------
     corrected: pandas.DataFrame
     """
-    corrector_samples_mask = classes.isin([corrector_class])
+    corrector_samples_mask = classes.isin([corrector_classes])
     scaling_factor = data[corrector_samples_mask].mean()
 
     def corrector_helper(x):
         corrected_batch = batch_correction(x, run_order.loc[x.index], classes,
-                                           corrector_class, sample_classes,
+                                           corrector_classes, process_classes,
                                            mode, **kwargs)
         corrected_batch = corrected_batch.divide(corrected_batch.mean())
         return corrected_batch
