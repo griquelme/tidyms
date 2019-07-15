b524c58f21ee282eb8d6148b4b37b1e5a66d37d8
Gabriel Riquelme <griquelme.chm@gmail.com>
2019-07-14 22:19:47 -0300

    Changed transform to process in pipeline method

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     data = mfv.process.data_container_from_excel("examples/example_sim.xlsx")
     config = mfv.process.read_config("examples/config.yaml")
     pipeline = ms_feature_validation.process.pipeline_from_list(config["Pipeline"])
-    pipeline.transform(data)
+    pipeline.process(data)
