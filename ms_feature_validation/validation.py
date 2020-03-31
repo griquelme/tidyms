62bd02aa8baced69fed0dbb2182a7253e918f65b
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-03-31 11:44:53 -0300

    functions for filter validation

--- ms_feature_validation/validation.py
+++ ms_feature_validation/validation.py
@@ -7,6 +7,67 @@ import warnings
 from os import getcwd
 from os.path import isdir
 from os.path import join
+import cerberus
+import copy
+
+# functions used by check_with
+def is_callable(field, value, error):
+    if not hasattr(value, "__call__"):
+        msg = "Must be a string or callable"
+        error(field, msg)
+# -----------------------------
+
+
+def validate(params: dict, validator: cerberus.Validator) -> None:
+    """
+    Function used to validate parameters.
+
+    Parameters
+    ----------
+    params: dict
+    validator: cerberus.Validator
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    ValueError: if any of the parameters are invalid.
+    """
+    pass_validation = validator.validate(params)
+    if not pass_validation:
+        msg = ""
+        for field, e_msgs in validator.errors.items():
+            for e_msg in e_msgs:
+                msg += "{} {}\n".format(field, e_msg)
+        raise ValueError(msg)
+
+
+class ValidatorWithLowerThan(cerberus.Validator):
+    def _validate_lower_than(self, other, field, value):
+        """
+        Tests if a value is lower than the value of other field.
+
+        The rule's arguments are validated against this schema:
+        {"type": "string"}
+        """
+        if (other not in self.document) or (self.document[other] is None):
+            return False
+        if value >= self.document[other]:
+            msg = "{}, must be lower than {}".format(field, other)
+            self._error(field, msg)
+
+    def _validate_is_positive(self, is_positive, field, value):
+        """
+        Tests if a value is positive
+
+        The rule's arguments are validated against this schema:
+        {"type": "boolean"}
+        """
+        if is_positive and (value is not None) and (value <= 0):
+            msg = "Must be a positive number"
+            self._error(field, msg)
 
 
 def validate_data_matrix(df):
@@ -122,3 +183,62 @@ def validate_blank_corrector_params(params):
         raise ValueError(msg)
 
 
+blank_corrector_schema = {"corrector_classes": {"type": "list",
+                                                "nullable": True,
+                                                "schema": {"type": "string"}},
+                          "process_classes": {"type": "list",
+                                              "nullable": True,
+                                              "schema": {"type": "string"}},
+                          "mode": {"anyof": [{"type": "string",
+                                              "allowed": ["lod", "loq",
+                                                          "mean", "max"]},
+                                             {"check_with": is_callable}]},
+                          "factor": {"type": "number",
+                                     "is_positive": True}
+                          }
+blankCorrectorValidator = ValidatorWithLowerThan(blank_corrector_schema)
+
+
+prevalence_filter_schema = {"lb": {"type": "number",
+                                   "min": 0,
+                                   "max": 1,
+                                   "lower_than": "ub"},
+                            "ub": {"type": "number",
+                                   "min": 0,
+                                   "max": 1},
+                            "threshold": {"type": "number",
+                                          "min": 0},
+                            "intraclass": {"type": "boolean"}
+                            }
+prevalence_filter_schema["process_classes"] = \
+    blank_corrector_schema["process_classes"]
+
+prevalenceFilterValidator = ValidatorWithLowerThan(prevalence_filter_schema)
+
+
+dratio_filter_schema = {"robust": {"type": "boolean"}}
+dratio_filter_schema["lb"] = prevalence_filter_schema["lb"]
+dratio_filter_schema["ub"] = prevalence_filter_schema["ub"]
+dRatioFilterValidator = ValidatorWithLowerThan(dratio_filter_schema)
+
+variation_filter_schema = copy.deepcopy(dratio_filter_schema)
+variation_filter_schema["process_classes"] = \
+    prevalence_filter_schema["process_classes"]
+variation_filter_schema["intraclass"] = prevalence_filter_schema["intraclass"]
+
+batch_corrector_schema = {"n_min": {"type": "integer",
+                                    "min": 4},
+                          "frac": {"type": "number",
+                                   "is_positive": True,
+                                   "max": 1,
+                                   "nullable": True},
+                          "interpolator": {"type": "string",
+                                           "allowed": ["splines", "linear"]}
+                          }
+batch_corrector_schema["threshold"] = prevalence_filter_schema["threshold"]
+batch_corrector_schema["process_classes"] = \
+    blank_corrector_schema["process_classes"]
+batch_corrector_schema["corrector_classes"] = \
+    blank_corrector_schema["corrector_classes"]
+
+batchCorrectorValidator = ValidatorWithLowerThan(batch_corrector_schema)
