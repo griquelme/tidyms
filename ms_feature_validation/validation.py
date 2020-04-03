ca327e597256c2a65eab564ec812bac66e6c253a
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-04-03 13:58:09 -0300

    added n_qc to batch_corrector_schema

--- ms_feature_validation/validation.py
+++ ms_feature_validation/validation.py
@@ -99,7 +99,7 @@ def validate_data_matrix(df):
                       "before further analysis")
 
 
-def validate_feature_definitions(df):
+def validate_feature_metadata(df):
     """
     Check type and values of rt and mz
     Parameters
@@ -121,7 +121,7 @@ def validate_feature_definitions(df):
         raise ValueError("mz values should be greater than zero")
 
 
-def validate_sample_information(df):
+def validate_sample_metadata(df):
     """
     Check sample class.
     Parameters
@@ -133,21 +133,13 @@ def validate_sample_information(df):
     """
     if "class" not in df.columns:
         raise KeyError("class information are required for all samples")
-    # if "batch" not in df.columns:
-    #     warnings.warn("No batch information available. Batch and order "
-    #                   "information is required for batch and interbatch "
-    #                   "correction.")
-    # if "order" not in df.columns:
-    #     warnings.warn("No order information available. Batch and order "
-    #                   "information is required for batch and interbatch "
-    #                   "correction.")
 
 
 def validate_data_container(data_matrix, feature_definitions,
                             sample_info, data_path):
     validate_data_matrix(data_matrix)
-    validate_feature_definitions(feature_definitions)
-    validate_sample_information(sample_info)
+    validate_feature_metadata(feature_definitions)
+    validate_sample_metadata(sample_info)
     samples_equal = data_matrix.index.equals(sample_info.index)
     if not samples_equal:
         msg = "Samples names should be equal in data matrix and sample info."
@@ -162,25 +154,6 @@ def validate_data_container(data_matrix, feature_definitions,
         full_path = join(getcwd(), data_path)
         msg = "{} not found".format(full_path)
         raise FileNotFoundError(msg)
-
-
-def validate_blank_corrector_params(params):
-    if not all([isinstance(x, str) for x in params["corrector_classes"]]):
-        msg = "`corrector_classes` should be a list of strings"
-        raise TypeError(msg)
-    if not all([isinstance(x, str) for x in params["process_classes"]]):
-        msg = "`process_classes` should be a list of strings"
-        raise TypeError(msg)
-    blank_relation_check = (isinstance(params["lb"], (float, int)) and
-                            (params["lb"] <= 1) and
-                            (params["lb"] >= 0) and
-                            (params["lb"] <= params["ub"]))
-    if not blank_relation_check:
-        msg = "`blank_relation` should be an scalar between 0 and 1."
-        raise ValueError(msg)
-    if not params["mode"] in ["mean", "max"]:
-        msg = "`mode` should be `max` or `mean`."
-        raise ValueError(msg)
 
 
 blank_corrector_schema = {"corrector_classes": {"type": "list",
@@ -233,6 +206,9 @@ batch_corrector_schema = {"n_min": {"type": "integer",
                                    "is_positive": True,
                                    "max": 1,
                                    "nullable": True},
+                          "n_qc": {"type": "integer",
+                                   "nullable": True,
+                                   "is_positive": True},
                           "interpolator": {"type": "string",
                                            "allowed": ["splines", "linear"]}
                           }
@@ -243,3 +219,44 @@ batch_corrector_schema["corrector_classes"] = \
     blank_corrector_schema["corrector_classes"]
 
 batchCorrectorValidator = ValidatorWithLowerThan(batch_corrector_schema)
+# TODO: validate DataContainer using cerberus
+
+
+def make_make_chromatogram_validator(ms_data):
+    n_spectra = ms_data.reader.getNrSpectra()
+    _make_chromatogram_schema = {"window": {"type": "number",
+                                            "nullable": True,
+                                            "is_positive": True},
+                                 "accumulator": {"type": "string",
+                                                 "allowed": ["mean", "sum"]},
+                                 "start": {"type": "integer",
+                                           "nullable": True,
+                                           "min": 0,
+                                           "max": n_spectra,
+                                           "lower_than": "end"},
+                                 "end": {"type": "integer",
+                                         "nullable": True,
+                                         "min": 0,
+                                         "max": n_spectra}
+                                 }
+    return ValidatorWithLowerThan(_make_chromatogram_schema)
+
+
+# validator for  CWT peak picking
+_cwt_schema = {"bl_ratio": {"type": "number",
+                            "is_positive": True},
+               "snr": {"type": "number",
+                       "is_positive": True},
+               "min_width": {"type": "number",
+                             "is_positive": True,
+                             "lower_than": "max_width"},
+               "max_width": {"type": "number",
+                             "is_positive": True},
+               "max_distance": {"type": "integer",
+                                "is_positive": True,
+                                "nullable": True},
+               "min_length": {"type": "integer",
+                              "is_positive": True,
+                              "nullable": True},
+               "gap_thresh": {"type": "integer",
+                              "min": 0}}
