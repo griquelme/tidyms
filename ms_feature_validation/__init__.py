2d0a86db880e47dcf53b697fd47bf1da537bfa88
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-05-19 15:47:29 -0300

    added docstring

--- ms_feature_validation/__init__.py
+++ ms_feature_validation/__init__.py
@@ -1,9 +1,22 @@
+"""
+The Mass Spectrometry Kit for Metabolomics (MSKM)
+=================================================
+
+A package to work with Mass Spectrometry data from Metabolomics Experiments.
+
+Provides
+    1. The MSData object to analyze and process raw MS data.
+    2. The DataContainer object to store metabolomics data sets.
+    3. Pipeline and Processor objects to perform curation of data sets.
+    4. Functions for feature detection and feature correspondence.
+"""
+
 from . import fileio
-from . import _filter_functions
 from . import data_container
 from . import utils
 from . import peaks
-from . import validation
 from . import filter
 from . import lcms
 from . import metabolomics
+from .data_container import DataContainer
+from .fileio import MSData
