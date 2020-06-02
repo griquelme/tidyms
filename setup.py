ef3bf93e542d8ef2a086c6b60557a0d266514f25
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-06-02 17:24:52 -0300

    added seaborn to the requirements

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ setup(name='ms_feature_validation',
       packages=['ms_feature_validation'],
       install_requires=['pandas>=0.24', 'pyopenms>=2.4', 'numpy>=1.15',
                         'pyyaml', 'statsmodels', 'scipy', 'scikit-learn',
-                        'bokeh', 'xlrd>=1.2', 'cerberus'])
+                        'bokeh', 'xlrd>=1.2', 'cerberus', 'seaborn'])
