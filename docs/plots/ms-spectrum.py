73ca17d8c16e4a938a2f3d67d221f9f2b6931785
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-06-17 15:13:55 -0300

    fixed plot

--- docs/plots/ms-spectrum.py
+++ docs/plots/ms-spectrum.py
@@ -10,5 +10,4 @@ sp_params = np.array([[401, 0.01, 100], [402, 0.01, 15], [403, 0.01, 2]])
 spint = mfv.utils.gaussian_mixture(mz, sp_params).sum(axis=0)
 
 spectrum = mfv.MSSpectrum(mz, spint)
-spectrum.find_peaks()
 spectrum.plot(fig_params={"height": 250})
