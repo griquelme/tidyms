a2bdd3f399092029e5543e5263314f608335b22f
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-06-20 11:43:02 -0300

    changed package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-setup(name='ms_feature_validation',
-      packages=['ms_feature_validation'],
+setup(name='tidyms',
+      packages=['tidyms'],
       install_requires=['pandas>=1.0', 'pyopenms>=2.4', 'numpy>=1.15',
                         'pyyaml', 'statsmodels', 'scipy', 'scikit-learn>=0.23',
                         'bokeh', 'xlrd>=1.2', 'cerberus', 'seaborn',
