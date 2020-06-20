6f962e7ec51b7b895e892101035e1eee093dec1f
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-06-20 11:11:59 -0300

    added version tequirements to packages. Added pytest to requirements.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 setup(name='ms_feature_validation',
       packages=['ms_feature_validation'],
-      install_requires=['pandas>=0.24', 'pyopenms>=2.4', 'numpy>=1.15',
-                        'pyyaml', 'statsmodels', 'scipy', 'scikit-learn',
-                        'bokeh', 'xlrd>=1.2', 'cerberus', 'seaborn'])
+      install_requires=['pandas>=1.0', 'pyopenms>=2.4', 'numpy>=1.15',
+                        'pyyaml', 'statsmodels', 'scipy', 'scikit-learn>=0.23',
+                        'bokeh', 'xlrd>=1.2', 'cerberus', 'seaborn',
+                        'pytest>=5.0'])
