7e2c494efedf9df909900d88ab80b81040ae8330
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-07-22 17:43:02 -0300

    added scipy=1.4 to requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 setup(name='tidyms',
       packages=['tidyms'],
       install_requires=['pandas>=1.0', 'pyopenms>=2.4', 'numpy>=1.15',
-                        'pyyaml', 'statsmodels', 'scipy', 'scikit-learn>=0.23',
+                        'pyyaml', 'statsmodels', 'scipy=1.4', 'scikit-learn>=0.23',
                         'bokeh', 'xlrd>=1.2', 'cerberus', 'seaborn',
                         'pytest>=5.0'])
