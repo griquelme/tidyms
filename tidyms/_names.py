c4ecb292dc2365dbf582838716b1c739237fe964
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-08-12 16:48:56 -0300

    add several definitions

--- tidyms/_names.py
+++ tidyms/_names.py
@@ -5,14 +5,16 @@ _sample_class = "class"
 _sample_id = "id"
 _sample_batch = "batch"
 _sample_order = "order"
-_sample_type = "sample"
-_qc_type = "qc"
-_blank_type = "blank"
-_suitability_type = "suitability"
+_sample_type = "type"
+_study_sample_type = "sample"
+_qc_sample_type = "qc"
+_blank_sample_type = "blank"
+_suitability_sample_type = "suitability"
 _zero_type = "zero"
-SAMPLE_TYPES = [_sample_type, _qc_type, _blank_type,
-                _suitability_type, _zero_type]
+SAMPLE_TYPES = [_study_sample_type, _qc_sample_type, _blank_sample_type,
+                _suitability_sample_type, _zero_type]
 
 __all__ = ["_raw_path", "_sample_class", "_sample_id", "_sample_batch",
-           "_sample_order", "_sample_type", "_qc_type", "_blank_type",
-           "_suitability_type", "_zero_type", "SAMPLE_TYPES"]
+           "_sample_order", "_sample_type","_study_sample_type",
+           "_qc_sample_type", "_blank_sample_type", "_suitability_sample_type",
+           "_zero_type", "SAMPLE_TYPES"]
