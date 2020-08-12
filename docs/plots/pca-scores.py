5a952e7dfab15e309a5039d892b94c0a4cd52d38
Gabriel Riquelme <griquelme.chm@gmail.com>
2020-08-12 17:29:19 -0300

    removed blank classes from the plot

--- docs/plots/pca-scores.py
+++ docs/plots/pca-scores.py
@@ -3,5 +3,7 @@ from bokeh import plotting
 plotting.output_file("example.html")
 
 data = ms.fileio.load_dataset("reference-materials")
-ignore = ["Z", "B", "SSS"]
-data.plot.pca_scores(fig_params={"height": 250})
+ignore = ["Z", "SV", "B", "SSS", "SCQC"]
+data.plot.pca_scores(fig_params={"height": 250},
+                     ignore_classes=ignore,
+                     scaling="autoscaling")
