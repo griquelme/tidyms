161cc84e69166bda415f0602b02184b49c24879d
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-03-15 09:45:02 -0300

    restructured code. Module now have only generic functions to plot lines and stems.

--- tidyms/_plot_bokeh.py
+++ tidyms/_plot_bokeh.py
@@ -1,125 +1,142 @@
 import numpy as np
 import bokeh.plotting
-from bokeh.palettes import Set3
+from bokeh.palettes import all_palettes
+from bokeh.models import ColumnDataSource, Segment
 from .peaks import Peak
-from typing import List, Optional
+from .utils import SETTINGS
+from typing import Dict, Generator, List, Optional
+
+BOKEH_SETTINGS = SETTINGS["bokeh"]
 
 
-def plot_chromatogram(rt: np.ndarray, spint: np.ndarray,
-                      peaks: Optional[List[Peak]], draw: bool = True,
-                      fig_params: Optional[dict] = None,
-                      line_params: Optional[dict] = None
-                      ) -> bokeh.plotting.Figure:
+def get_theme_params() -> dict:
+    return BOKEH_SETTINGS["theme"]
+
+
+def get_line_params() -> dict:
+    return BOKEH_SETTINGS["line"]
+
+
+def get_chromatogram_figure_params() -> dict:
+    return BOKEH_SETTINGS["chromatogram"]["figure"]
+
+
+def get_spectrum_figure_params() -> dict:
+    return BOKEH_SETTINGS["spectrum"]["figure"]
+
+
+def get_varea_params() -> dict:
+    return BOKEH_SETTINGS["varea"]
+
+
+def get_palette() -> List[str]:
+    palette_params = BOKEH_SETTINGS["palette"]
+    return find_palette(**palette_params)
+
+
+def make_figure(fig_params: Optional[dict]):
+    if fig_params is None:
+        fig_params = dict()
+    return bokeh.plotting.figure(**fig_params)
+
+
+def find_palette(name: str, size: Optional[int] = None) -> List[str]:
+    try:
+        palette = bokeh.palettes.all_palettes[name]
+        # by default get the palette with the largest size
+        if size is None:
+            size = max(list(palette.keys()))
+        palette = palette[size]
+    except KeyError:
+        link = "https://docs.bokeh.org/en/latest/docs/reference/palettes.html"
+        msg = "Palette not found. Refer to the list of prebuilt palettes at {}"
+        raise ValueError(msg.format(link))
+    return palette
+
+
+def palette_cycler(palette: List[str]) -> Generator[str, None, None]:
+    ind = 0
+    size = len(palette)
+    while True:
+        yield palette[ind]
+        ind = (ind + 1) % size
+
+
+def add_line(
+    figure: bokeh.plotting.Figure,
+    x: np.ndarray,
+    y: np.ndarray,
+    line_params: Optional[dict] = None
+):
     """
-    Plots a chromatogram
+    Plots a line.
 
     Parameters
     ----------
-    rt : array
-        array of retention time
-    spint : array
-        array of intensity
-    peaks : List[Peaks]
-    draw : bool, optional
-        if True run bokeh show function.
-    fig_params : dict
+    figure : bokeh.plotting.Figure
         key-value parameters to pass into bokeh figure function.
+    x : array
+    y : array
     line_params : dict
         key-value parameters to pass into bokeh line function.
 
-    Returns
-    -------
-    bokeh Figure
-
     """
-    default_line_params = {"line_width": 1, "line_color": "black", "alpha": 0.8}
-    default_fig_params = {"aspect_ratio": 1.5}
-    cmap = Set3[12]
-
-    if line_params is None:
-        line_params = default_line_params
-    else:
-        for params in line_params:
-            default_line_params[params] = line_params[params]
-        line_params = default_line_params
-
-    if fig_params is None:
-        fig_params = default_fig_params
-    else:
-        default_fig_params.update(fig_params)
-        fig_params = default_fig_params
-
-    fig = bokeh.plotting.figure(**fig_params)
-    fig.line(rt, spint, **line_params)
-    if peaks is not None:
-        for k, peak in enumerate(peaks):
-            fig.varea(rt[peak.start:peak.end], spint[peak.start:peak.end],
-                      0, fill_alpha=0.8, fill_color=cmap[k % 12])
-            # k % 12 is used to cycle over the colormap
-
-    #  figure appearance
-    fig.xaxis.axis_label = "Rt [s]"
-    fig.yaxis.axis_label = "intensity [au]"
-    fig.yaxis.axis_label_text_font_style = "bold"
-    fig.yaxis.formatter.precision = 2
-    fig.xaxis.axis_label_text_font_style = "bold"
-
-    if draw:
-        bokeh.plotting.show(fig)
-    return fig
+    default_line_params = get_line_params()
+    if line_params:
+        default_line_params.update(line_params)
+    line_params = default_line_params
+    figure.line(x, y, **line_params)
 
 
-def plot_ms_spectrum(mz: np.ndarray, spint: np.ndarray,
-                     draw: bool = True, fig_params: Optional[dict] = None,
-                     line_params: Optional[dict] = None
-                     ) -> bokeh.plotting.Figure:
-    """
-    Plot a mass spectrum.
+def set_chromatogram_axis_params(fig: bokeh.plotting.Figure):
+    xaxis_params = BOKEH_SETTINGS["chromatogram"]["xaxis"]
+    yaxis_params = BOKEH_SETTINGS["chromatogram"]["yaxis"]
+    fig.xaxis.update(**xaxis_params)
+    fig.yaxis.update(**yaxis_params)
 
-    Parameters
-    ----------
-    mz : array
-        array of m/z
-    spint : array
-        array of intensities
-    draw : bool, optional
-        if True run bokeh show function.
-    fig_params : dict
-        key-value parameters to pass into bokeh figure function.
-    line_params : dict
-        key-value parameters to pass into bokeh line function.
 
-    Returns
-    -------
-    bokeh Figure
-    """
-    default_line_params = {"line_width": 1, "line_color": "black",
-                           "alpha": 0.8}
+def set_ms_spectrum_axis_params(fig: bokeh.plotting.Figure):
+    xaxis_params = BOKEH_SETTINGS["spectrum"]["xaxis"]
+    yaxis_params = BOKEH_SETTINGS["spectrum"]["yaxis"]
+    fig.xaxis.update(**xaxis_params)
+    fig.yaxis.update(**yaxis_params)
 
-    if line_params is None:
-        line_params = default_line_params
-    else:
-        for params in line_params:
-            default_line_params[params] = line_params[params]
-        line_params = default_line_params
 
-    default_fig_params = {"aspect_ratio": 1.5}
-    if fig_params is None:
-        fig_params = default_fig_params
-    else:
-        default_fig_params.update(fig_params)
-        fig_params = default_fig_params
+def fill_peaks(
+    figure: bokeh.plotting.Figure,
+    x: np.ndarray,
+    y: np.ndarray,
+    peaks: List[Peak],
+    palette: Optional[str] = None,
+    varea_params: Optional[Dict] = None,
+):
+    default_varea_params = get_varea_params()
+    if varea_params:
+        default_varea_params.update(varea_params)
+    varea_params = default_varea_params
 
-    fig = bokeh.plotting.figure(**fig_params)
-    fig.line(mz, spint, **line_params)
+    if palette is None:
+        palette = get_palette()
+    colors = palette_cycler(palette)
+    for p in peaks:
+        color = next(colors)
+        xp = x[p.start:p.end]
+        yp = y[p.start:p.end]
+        figure.varea(xp, yp, 0, fill_color=color, **varea_params)
 
-    #  figure appearance
-    fig.xaxis.axis_label = "m/z"
-    fig.yaxis.axis_label = "intensity [au]"
-    fig.yaxis.axis_label_text_font_style = "bold"
-    fig.yaxis.formatter.precision = 2
-    fig.xaxis.axis_label_text_font_style = "bold"
 
-    if draw:
-        bokeh.plotting.show(fig)
-    return fig
+def add_stems(
+    fig: bokeh.plotting.Figure,
+    x: np.ndarray,
+    y: np.ndarray,
+    line_params: Optional[Dict] = None
+):
+    default_line_params = get_line_params()
+    if line_params:
+        default_line_params.update(line_params)
+    line_params = default_line_params
+    x0 = x
+    y0 = np.zeros_like(y)
+    source = ColumnDataSource(dict(x0=x0, x1=x, y0=y0, y1=y))
+    stems = Segment(x0="x0", x1="x1", y0="y0", y1="y1", **line_params)
+    fig.add_glyph(source, stems)
