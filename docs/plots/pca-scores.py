decd6d5569072b46acf7c924f770e188675b1115
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-03-17 17:50:55 -0300

    removed old versions of bokeh plots.

--- docs/plots/pca-scores.py
+++
@@ -1,9 +0,0 @@
-import tidyms as ms
-from bokeh import plotting
-plotting.output_file("example.html")
-
-data = ms.fileio.load_dataset("reference-materials")
-ignore = ["Z", "SV", "B", "SSS", "SCQC"]
-data.plot.pca_scores(fig_params={"height": 250},
-                     ignore_classes=ignore,
-                     scaling="autoscaling")
