3a5b70988a263fe3c40265eba6536c91b6166594
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-03-17 17:49:55 -0300

    moved to bokeh directory

--- docs/bokeh/feature-plot.py
+++ docs/bokeh/feature-plot.py
@@ -1,8 +1,9 @@
 import tidyms as ms
 from bokeh import plotting
 import numpy as np
-plotting.output_file("example.html")
-
+import sys
+out = sys.argv[-1]
+plotting.output_file(out)
 data = ms.fileio.load_dataset("reference-materials")
 
 # search [M+H]+ from trp in the features
@@ -11,4 +12,5 @@ rt = 124
 # get a list of features compatible with the given m/z and rt
 ft_name = data.select_features(mz, rt)
 
-data.plot.feature(ft_name[0])
+f = data.plot.feature(ft_name[0], draw=False)
+plotting.save(f)
