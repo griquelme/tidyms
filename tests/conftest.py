4e920afeb969cc2e15fcacf225084f4c9a2e7758
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-05-31 12:34:06 -0300

    modified session start code

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,68 +7,12 @@ from tidyms.utils import get_tidyms_path
 import numpy as np
 import pytest
 import os
-import requests
-
-
-def download_test_data(name, files):
-    """Download a dataset from GitHub"""
-    cache_path = fileio.get_tidyms_path()
-    dataset_path = os.path.join(cache_path, name)
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
-    url = "https://raw.githubusercontent.com/griquelme/tidyms-data/master/"
-    dataset_url = url + "/" + name
-
-    WINDOWS_LINE_ENDING = b'\r\n'
-    UNIX_LINE_ENDING = b'\n'
-
-    for f in files:
-        file_path = os.path.join(dataset_path, f)
-        if not os.path.isfile(file_path):
-            file_url = dataset_url + "/" + f
-            r = requests.get(file_url)
-            # save content
-            with open(file_path, "w") as fin:
-                fin.write(r.text)
-
-            # convert end line characters for windows
-            if os.name == "nt":
-                with open(file_path, "rb") as fin:
-                    content = fin.read()
-                content = content.replace(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING)
-
-                with open(file_path, "wb") as fin:
-                    fin.write(content)
-
-
-def download_raw_test_data():
-    """Download mzML files for testing"""
-    name = "test-raw-data"
-    files = [
-        "centroid-data-indexed-uncompressed.mzML",
-        "centroid-data-zlib-indexed-compressed.mzML",
-        "centroid-data-zlib-no-index-compressed.mzML",
-        "profile-data-zlib-indexed-compressed.mzML"
-    ]
-    download_test_data(name, files)
-
-
-def download_nist_test_data():
-    name = "test-nist-raw-data"
-    files1 = ["NZ_20200226_{:03d}.mzML".format(x) for x in range(1, 24, 2)]
-    files2_n = [
-        9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 39, 49, 59, 69, 79, 89, 91,
-        93, 97
-    ]
-    files2 = ["NZ_20200227_{:03d}.mzML".format(x) for x in files2_n]
-    files = files1 + files2
-    download_test_data(name, files)
 
 
 # simulated data used for tests
 def pytest_sessionstart(session):
-    download_raw_test_data()
-    download_nist_test_data()
+    for dataset in fileio.list_available_datasets(False):
+        fileio.download_dataset(dataset)
 
 
 @pytest.fixture
