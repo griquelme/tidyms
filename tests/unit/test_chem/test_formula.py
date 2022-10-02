f20657a72da7e68767b8cb852f55728c6c41a5f1
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-10-02 11:55:57 -0300

    test: 100% coverage.

--- tests/unit/test_chem/test_formula.py
+++ tests/unit/test_chem/test_formula.py
@@ -1,129 +1,123 @@
 from tidyms.chem import formula
-from tidyms.chem import PTABLE
-from tidyms.chem.atoms import InvalidIsotope
+from tidyms.chem.atoms import InvalidIsotope, PeriodicTable
 import pytest
 
 
-# test find_matching parenthesis
-
-find_matching_parenthesis_valid_input = \
-    [("[Cr[H2O]6]3+", 0, 9), ("[C9H11NO2]", 0, 9),
-     ("C9H11N(17O)2", 6, 10), ("[Cr[(2H)2O]6]3+", 3, 10)]
-
-@pytest.mark.parametrize("formula_str,p_open,p_close",
-                         find_matching_parenthesis_valid_input)
+@pytest.mark.parametrize(
+    "formula_str,p_open,p_close",
+    [
+        ("[Cr[H2O]6]3+", 0, 9),
+        ("[C9H11NO2]", 0, 9),
+        ("C9H11N(17O)2", 6, 10),
+        ("[Cr[(2H)2O]6]3+", 3, 10),
+    ],
+)
 def test_find_matching_parenthesis_valid_input(formula_str, p_open, p_close):
     test_p_close = formula._find_matching_parenthesis(formula_str, p_open)
     assert test_p_close == p_close
 
 
-## test parse charge
-
-valid_formulas_charge = [("H2O", "H2O", 0), ("(13C)", "(13C)", 0),
-                         ("[CO3]2-", "CO3", -2),
-                         ("[Cr[H2O]6]3+", "Cr[H2O]6", 3),
-                         ("[C9H11NO2]", "[C9H11NO2]", 0), ("CO-", "CO", -1),
-                         ("[H2O]+", "[H2O]", 1), ("H2O+", "H2O", 1)]
-
-invalid_formulas_charge = ["SO42-"]
-
-
-@pytest.mark.parametrize("formula_str,formula_without_charge,charge",
-                         valid_formulas_charge)
+@pytest.mark.parametrize(
+    "formula_str,formula_without_charge,charge",
+    [
+        ("H2O", "H2O", 0),
+        ("(13C)", "(13C)", 0),
+        ("[CO3]2-", "CO3", -2),
+        ("[Cr[H2O]6]3+", "Cr[H2O]6", 3),
+        ("[C9H11NO2]", "[C9H11NO2]", 0),
+        ("CO-", "CO", -1),
+        ("[H2O]+", "[H2O]", 1),
+        ("H2O+", "H2O", 1),
+    ],
+)
 def test_parse_charge_valid_input(formula_str, formula_without_charge, charge):
     test_formula_without_charge, q = formula._parse_charge(formula_str)
     assert test_formula_without_charge == formula_without_charge
     assert charge == q
 
 
-@pytest.mark.parametrize("formula_str", invalid_formulas_charge)
+@pytest.mark.parametrize("formula_str", ["SO42-"])
 def test_parse_charge_invalid_input(formula_str):
     with pytest.raises(formula.InvalidFormula):
         _, q = formula._parse_charge(formula_str)
 
-# test get_token_type
 
-get_token_type_input = [("H2O", 0, 0,), ("H2(34S)O4", 2, 1),
-                        ("[Cr(H2O)6]3+", 3, 2), ("[Fe[CN]6]4-", 3, 2)]
-
-@pytest.mark.parametrize("formula_str,ind,token_type", get_token_type_input)
+@pytest.mark.parametrize(
+    "formula_str,ind,token_type",
+    [
+        ("H2O", 0, 0),
+        ("H2(34S)O4", 2, 1),
+        ("[Cr(H2O)6]3+", 3, 2),
+        ("[Fe[CN]6]4-", 3, 2),
+    ],
+)
 def test_get_token_type(formula_str, ind, token_type):
     test_token_type = formula._get_token_type(formula_str, ind)
     assert test_token_type == token_type
 
 
-# test get_coefficient
-get_coefficient_valid_input = [("H2O", 3, 1, 3), ("CO2", 1, 1, 1),
-                               ("C9H11NO2", 3, 11, 5)]
-
-@pytest.mark.parametrize("formula_str,ind,coeff,new_ind",
-                         get_coefficient_valid_input)
+@pytest.mark.parametrize(
+    "formula_str,ind,coeff,new_ind",
+    [
+        ("H2O", 3, 1, 3),
+        ("CO2", 1, 1, 1),
+        ("C9H11NO2", 3, 11, 5),
+    ]
+)
 def test_get_coefficient_valid_input(formula_str, ind, coeff, new_ind):
     test_coeff, test_ind = formula._get_coefficient(formula_str, ind)
     assert coeff == test_coeff
     assert test_ind == new_ind
 
-# test tokenize element
-tokenize_element_test_input = [("H2O", 0, 2, "H"),
-                               ("H2O", 2, 3, "O"),
-                               ("C9H11NO2", 5, 6, "N"),
-                               ("C9H11N(17O)2", 5, 6, "N"),
-                               ("Cr(H2O)6", 0, 2, "Cr")]
 
-@pytest.mark.parametrize("formula_str,ind,new_ind,element",
-                         tokenize_element_test_input)
+@pytest.mark.parametrize(
+    "formula_str,ind,new_ind,element",
+    [
+        ("H2O", 0, 2, "H"),
+        ("H2O", 2, 3, "O"),
+        ("C9H11NO2", 5, 6, "N"),
+        ("C9H11N(17O)2", 5, 6, "N"),
+        ("Cr(H2O)6", 0, 2, "Cr"),
+    ]
+)
 def test_tokenize_element_valid_input(formula_str, ind, new_ind, element):
     token, test_index = formula._tokenize_element(formula_str, ind)
     assert test_index == new_ind
-    isotope = PTABLE[element].get_most_abundant_isotope()
+    isotope = PeriodicTable().get_element(element).get_monoisotope()
     assert isotope in token
 
 
-# test tokenize isotope
-
-tokenize_isotope_test_input = [("(13C)O2", 0, "C", 13, 5),
-                               ("C9H11(15N)2O2", 5, "N", 15, 11),
-                               ("C6H12O5(18O)", 7, "O", 18, 12),
-                               ("C6H12O4(18O)2", 7, "O", 18, 13)]
-
-@pytest.mark.parametrize("formula_str,ind,symbol,mass_number,new_ind",
-                         tokenize_isotope_test_input)
-def test_tokenize_isotope_valid_input(formula_str, ind, symbol,
-                                      mass_number, new_ind):
+@pytest.mark.parametrize(
+    "formula_str,ind,isotope_str,new_ind",
+    [
+        ("(13C)O2", 0, "13C", 5),
+        ("C9H11(15N)2O2", 5, "15N", 11),
+        ("C6H12O5(18O)", 7, "18O", 12),
+        ("C6H12O4(18O)2", 7, "18O", 13),
+    ]
+)
+def test_tokenize_isotope_valid_input(formula_str, ind, isotope_str, new_ind):
     token, test_index = formula._tokenize_isotope(formula_str, ind)
-    isotope = PTABLE[symbol].isotopes[mass_number]
+    isotope = PeriodicTable().get_isotope(isotope_str)
     assert test_index == new_ind
     assert isotope in token
 
 
-# test tokenize formula
-
-@pytest.fixture
-def tokenize_formula_input():
-    formula_input = \
-        [("H2O", {PTABLE["H"].isotopes[1]: 2,
-                  PTABLE["O"].isotopes[16]: 1}),
-         ("(13C)O2", {PTABLE["C"].isotopes[13]: 1,
-                      PTABLE["O"].isotopes[16]: 2}),
-         ("C9H11(15N)2O2", {PTABLE["C"].isotopes[12]: 9,
-                            PTABLE["H"].isotopes[1]: 11,
-                            PTABLE["N"].isotopes[15]: 2,
-                            PTABLE["O"].isotopes[16]: 2}),
-         ("C9H11N2O2", {PTABLE["C"].isotopes[12]: 9,
-                        PTABLE["H"].isotopes[1]: 11,
-                        PTABLE["N"].isotopes[14]: 2,
-                        PTABLE["O"].isotopes[16]: 2}),
-         ("Cr[(2H)2O]6", {PTABLE["Cr"].isotopes[52]: 1,
-                           PTABLE["H"].isotopes[2]: 12,
-                           PTABLE["O"].isotopes[16]: 6}),
-         ]
-    return formula_input
-
-def test_tokenize_formula(tokenize_formula_input):
-    for f, composition in tokenize_formula_input:
-        test_composition = formula._parse_formula(f)
-        for isotope in composition:
-            assert composition[isotope] == test_composition[isotope]
+@pytest.mark.parametrize(
+    "f_str,composition",
+    [
+        ("H2O", {"1H": 2, "16O": 1}),
+        ("(13C)O2", {"13C": 1, "16O": 2}),
+        ("C9H11(15N)2O2", {"12C": 9, "1H": 11, "15N": 2, "16O": 2}),
+        ("C9H11N2O2", {"12C": 9, "1H": 11, "14N": 2, "16O": 2}),
+        ("Cr[(2H)2O]6", {"52Cr": 1, "2H": 12, "16O": 6})
+    ]
+)
+def test_tokenize_formula(f_str, composition):
+    composition = {PeriodicTable().get_isotope(k): v for k, v in composition.items()}
+    test_composition = formula._parse_formula(f_str)
+    for isotope in composition:
+        assert composition[isotope] == test_composition[isotope]
 
 
 def test_arg_sort_elements():
@@ -133,88 +127,45 @@ def test_arg_sort_elements():
     assert sorted_ind == formula._arg_sort_elements(symbols, a)
 
 
-symbol_to_subformula_string_params = {
-    ("C", 12, 1, True, "C"), ("C", 12, 3, True, "C3"),
-    ("C", "13", 1, False, "(13C)"), ("C", 13, 3, False, "(13C)3")
-}
-
-@pytest.mark.parametrize("symbol,mass,coefficient,include_mass,subformula",
-                         symbol_to_subformula_string_params)
-def test_symbol_to_subformula_string(symbol, mass, coefficient, include_mass,
-                                     subformula):
-    test_subformula = \
-        formula._symbol_to_subformula_str(symbol, mass, coefficient,
-                                          is_most_abundant=include_mass)
-    assert subformula == test_subformula
-
-
-def test_composition_to_list():
-    composition = formula.Formula("H(13C)5N3C12").composition
-    symbols = ["C", "C", "H", "N"]
-    a = [12, 13, 1, 14]
-    coefficients = [12, 5, 1, 3]
-    is_most_abundant = [True, False, True, True]
-    test_symbols, test_a, test_coefficients, test_is_most_abundant = \
-        formula._composition_to_list(composition)
-    assert test_symbols == symbols
-    assert test_a == a
-    assert test_coefficients == coefficients
-    assert test_is_most_abundant == is_most_abundant
-
-
-ch_string_params = [("C6H12O6", "C6H12"), ("H6C3O4N3P4", "C3H6"),
-                    ("C6(13C)H7O8NPS", "C6(13C)H7"),
-                    ("(13C)4C7(2H)5NOP6", "C7(13C)4(2H)5")]
-
-@pytest.mark.parametrize("f_str,ch_str", ch_string_params)
-def test_get_ch_string(f_str, ch_str):
-    composition = formula.Formula(f_str).composition
-    symbols, a, coefficients, is_most_abundant = \
-        formula._composition_to_list(composition)
-    test_ch_str = formula._get_ch_string(symbols, a, coefficients,
-                                         is_most_abundant)
-    assert test_ch_str == ch_str
-
-
-heteroatom_string_params = [("C6H12O6", "C6H12O6"),
-                            ("H6C3O4N3P4", "C3H6N3O4P4"),
-                            ("C6(13C)H7O8NPS", "C6(13C)H7NO8PS"),
-                            ("(13C)4C7(2H)5NOP6", "C7(13C)4(2H)5NOP6")]
-
-@pytest.mark.parametrize("f_str,heteroatom_str", heteroatom_string_params)
-def test_get_ch_string(f_str, heteroatom_str):
-    composition = formula.Formula(f_str).composition
-    symbols, a, coefficients, is_most_abundant = \
-        formula._composition_to_list(composition)
-    test_heteroatom_str = formula._get_heteroatom_str(symbols, a, coefficients,
-                                                      is_most_abundant)
-    assert test_heteroatom_str == heteroatom_str
-
-@pytest.mark.parametrize("charge,charge_str",
-                         [(1, "+"), (2, "2+"), (-1, "-"), (-4, "4-")])
+@pytest.mark.parametrize(
+    "charge,charge_str",
+    [
+        (1, "+"),
+        (2, "2+"),
+        (-1, "-"),
+        (-4, "4-")
+    ]
+)
 def test_get_charge_str(charge, charge_str):
     test_charge_str = formula._get_charge_str(charge)
     assert test_charge_str == charge_str
 
 
-# test get_formula_str
-formula_str_params = [(formula.Formula("CO2"), "CO2"),
-                      (formula.Formula("(13C)C2H6O3"), "C2(13C)H6O3"),
-                      (formula.Formula("C24H46SPN(18O)2"), "C24H46N(18O)2PS"),
-                      (formula.Formula("[Cr(H2O)6]3+"), "[H12CrO6]3+"),
-                      (formula.Formula("CH3CH2CH3"), "C3H8"),
-                      (formula.Formula("F2"), "F2")]
-
-@pytest.mark.parametrize("f,f_str", formula_str_params)
+@pytest.mark.parametrize(
+    "f,f_str",
+    [
+        (formula.Formula("CO2"), "CO2"),
+        (formula.Formula("(13C)C2H6O3"), "C2(13C)H6O3"),
+        (formula.Formula("C24H46SPN(18O)2"), "C24H46N(18O)2PS"),
+        (formula.Formula("[Cr(H2O)6]3+"), "[H12CrO6]3+"),
+        (formula.Formula("CH3CH2CH3"), "C3H8"),
+        (formula.Formula("F2"), "F2"),
+    ]
+)
 def test_get_formula_str(f, f_str):
     test_f_str = str(f)
     assert test_f_str == f_str
 
 
-invalid_formulas = ["(CO2", "C(3H)4", "GCH2", "#H2O"]
-@pytest.mark.parametrize("f_str", invalid_formulas)
+@pytest.mark.parametrize("f_str", ["(CO2", "#H2O"])
 def test_parse_formula_invalid_formula(f_str):
-    with pytest.raises((formula.InvalidFormula, InvalidIsotope)):
+    with pytest.raises(formula.InvalidFormula):
+        formula.Formula(f_str)
+
+
+@pytest.mark.parametrize("f_str", ["(14C)O2", "(3H)2O"])
+def test_parse_formula_invalid_isotope(f_str):
+    with pytest.raises(InvalidIsotope):
         formula.Formula(f_str)
 
 
@@ -243,12 +194,76 @@ def test_formula_from_dictionary():
     charge = 1
     f = formula.Formula(composition, charge)
     for k in composition:
-        assert formula.find_isotope(k) in f.composition
+        assert PeriodicTable().get_isotope(k) in f.composition
     assert charge == f.charge
 
-formula_from_dictionary_test_values = \
-    [[{"C": 1, "G": 4}, 1], [{"C": -1, "H": 4}, 1], [{"C": 1, "H": 4}, 0.5]]
-@pytest.mark.parametrize("d,q", formula_from_dictionary_test_values)
-def test_formula_from_dictionary_bad_input(d, q):
-    with pytest.raises((InvalidIsotope, ValueError)):
-        formula.Formula(d, q)
+
+def test_formula_from_dictionary_invalid_isotope():
+    composition = {"C": 1, "G": 4}
+    charge = 1
+    with pytest.raises(InvalidIsotope):
+        formula.Formula(composition, charge)
+
+
+def test_formula_from_dictionary_invalid_isotope_type():
+    composition = {4: 1, "G": 4}
+    charge = 1
+    with pytest.raises(ValueError):
+        formula.Formula(composition, charge)
+
+
+@pytest.mark.parametrize(
+    "composition,q",
+    [
+        [{"C": -1, "H": 4}, 1],
+        [{"C": 1, "H": 4}, 0.5],
+    ]
+)
+def test_formula_from_dictionary_invalid_coefficient(composition, q):
+    with pytest.raises(ValueError):
+        formula.Formula(composition, q)
+
+
+def test_Formula_add():
+    f1 = formula.Formula("H2O")
+    f2 = formula.Formula("CO2")
+    f_sum = f1 + f2
+    expected = formula.Formula("H2CO3")
+    assert expected == f_sum
+
+
+def test_Formula_add_invalid_type():
+    f1 = formula.Formula("H2O")
+    f2 = "CO2"
+    with pytest.raises(ValueError):
+        f1 + f2
+
+
+def test_Formula_subtract_valid():
+    f1 = formula.Formula("C6H12O6")
+    f2 = formula.Formula("CO2")
+    f_diff = f1 - f2
+    expected = formula.Formula("C5H12O4")
+    assert expected == f_diff
+
+
+def test_Formula_subtract_invalid_type():
+    f1 = formula.Formula("C6H12O6")
+    f2 = "CO2"
+    with pytest.raises(ValueError):
+        f1 - f2
+
+
+def test_Formula_subtract_valid_zero_coeff():
+    f1 = formula.Formula("C4H8O2")
+    f2 = formula.Formula("CO2")
+    f_diff = f1 - f2
+    expected = formula.Formula("C3H8")
+    assert expected == f_diff
+
+
+def test_Formula_subtract_invalid_coeff():
+    f1 = formula.Formula("C4H8O")
+    f2 = formula.Formula("CO2")
+    with pytest.raises(ValueError):
+        f1 - f2
