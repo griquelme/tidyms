2a66a3c9cc047b7a2237cba2da2601fe01fb45eb
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-10-03 12:29:13 -0300

    Update imports.

--- tidyms/chem/__init__.py
+++ tidyms/chem/__init__.py
@@ -2,21 +2,29 @@
 Chemistry
 =========
 
-Provides
-    1. A Formula object to compute the exact mass and isotopic distribution of
-        molecular formulas.
-    2. A "Periodic Table" with elements and isotopes information.
-    3. A formula generator object to search molecular formulas based on exact
-        mass values.
-    4. An isotopic envelope scorer that scores the similarity between
-        experimental and theoretical isotopic distributions.
+Provides:
+
+1. A Formula object to compute the exact mass and isotopic distribution of molecular formulas.
+2. A PeriodicTable with element and isotope information.
+3. A formula generator object to search molecular formulas based on exact mass values.
+4. An EnvelopeScorer that scores the similarity between experimental and theoretical isotopic envelopes.
+
+Objects
+-------
+- PeriodicTable
+- Formula
+- FormulaGenerator
+- EnvelopeScorer
+
+Constants
+---------
+- EM : electron mass
+
 """
 
-from .formula_generator import FormulaGenerator
-from .isotope_scorer import IsotopeScorer, EnvelopeValidator
+from ._formula_generator import FormulaGenerator
+from .envelope_tools import EnvelopeScorer, EnvelopeValidator
 from .formula import Formula
 from .envelope_finder import EnvelopeFinder
 from .mmi_finder import MMIFinder
-from .atoms import PTABLE, EM
-from . import _isotope_distributions
-from . import mmi_finder
+from .atoms import EM, PeriodicTable
