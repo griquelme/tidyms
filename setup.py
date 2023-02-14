697e179b9a1c0ce3e39808a34cc8e4145dcb0936
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-02-14 16:14:07 -0300

    Update version number.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 PACKAGE_NAME = "tidyms"
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 LICENSE = 'BSD (3-clause)'
 AUTHOR = "Bioanalytical Mass Spectrometry Group at CIBION-CONICET"
 AUTHOR_EMAIL = "griquelme.chm@gmail.com"
