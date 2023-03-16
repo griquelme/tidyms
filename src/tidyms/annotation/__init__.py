04a531eabaee5a1392a7d3da23fb5646597728a6
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-03-16 14:23:38 -0300

    refactor: Add __init__ file for annotation module.

---
+++ src/tidyms/annotation/__init__.py
@@ -0,0 +1,21 @@
+"""
+Annotation
+----------
+
+Tools for feature annotation.
+
+Provides:
+
+1. Tools for isotopologue annotation.
+
+Functions
+---------
+annotate_isotopologues
+
+Objects
+-------
+IsotopologueAnnotator
+
+"""
+
+from .annotation import annotate, IsotopologueAnnotator
