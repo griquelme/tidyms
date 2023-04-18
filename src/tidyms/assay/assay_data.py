02b94d02120517dcd14c0a96dc5f41f890054288
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-04-18 09:18:28 -0300

    feat: WIP: AssayData class.

--- src/tidyms/assay/assay_data.py
+++ src/tidyms/assay/assay_data.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass, asdict
 from typing import Optional, Sequence, Type, Union
 from pathlib import Path
-from sqlalchemy import Float, ForeignKey, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table, MetaData
 from sqlalchemy import create_engine, delete, select
-from sqlalchemy.orm import declarative_base, sessionmaker, Mapped, mapped_column
+from sqlalchemy.orm import declarative_base, sessionmaker, Mapped, mapped_column, relationship
 from sqlalchemy.exc import IntegrityError
-from ..lcms import Feature, Roi
+from ..lcms import Annotation, Feature, Roi
+import json
 
 # TODO: get sample by processing stage
-# TODO: get ROI with and without features.
 # TODO: Idea: Assays are created using create_assay and load_assay functions.
 
 
@@ -19,7 +19,7 @@ class Sample:
     ms_level: int = 1
     start: Optional[float] = None
     end: Optional[float] = None
-    group: Optional[str] = None
+    group: str = ""
 
     def __post_init__(self):
         if isinstance(self.path, str):
@@ -34,26 +34,102 @@ class Sample:
         return d
 
 
+Base = declarative_base()
+
+
+class ProcessParameterModel(Base):
+    __tablename__ = "parameters"
+    step: Mapped[str] = mapped_column(String, primary_key=True)
+    parameters: Mapped[str] = mapped_column(String)
+
+
+class SampleModel(Base):
+    __tablename__ = "samples"
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    path: Mapped[str] = mapped_column(String, nullable=False)
+    ms_level: Mapped[int] = mapped_column(Integer)
+    start: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    end: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    group: Mapped[str] = mapped_column(String, nullable=True)
+    rois: Mapped[list["RoiModel"]] = relationship(back_populates="samples")
+    features: Mapped[list["FeatureModel"]] = relationship(back_populates="samples")
+
+
+class ProcessedSampleModel(Base):
+    __tablename__ = "processed_samples"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    sample_id: Mapped[str] = mapped_column(String, ForeignKey("samples.id"))
+    step: Mapped[str] = mapped_column(String, ForeignKey("parameters.step"))
+
+
+class RoiModel(Base):
+    __tablename__ = "rois"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    sample_id: Mapped[str] = mapped_column(ForeignKey("samples.id"))
+    data: Mapped[str] = mapped_column(String)
+    samples: Mapped["SampleModel"] = relationship(back_populates="rois")
+    features: Mapped[list["FeatureModel"]] = relationship(
+        back_populates="roi", lazy="immediate"
+    )
+    annotations: Mapped[list["AnnotationModel"]] = relationship(
+        back_populates="roi", lazy="immediate"
+    )
+
+
+class FeatureModel(Base):
+    __tablename__ = "features"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    sample_id: Mapped[str] = mapped_column(ForeignKey("samples.id"))
+    roi_id: Mapped[int] = mapped_column(ForeignKey("rois.id"))
+    data: Mapped[str] = mapped_column(String)
+    roi: Mapped["RoiModel"] = relationship(back_populates="features")
+    samples: Mapped["SampleModel"] = relationship(back_populates="features")
+    annotation: Mapped["AnnotationModel"] = relationship(back_populates="feature")
+
+
+class AnnotationModel(Base):
+    __tablename__ = "annotations"
+    id: Mapped[int] = mapped_column(
+        ForeignKey("features.id"), primary_key=True, autoincrement=True
+    )
+    sample_id: Mapped[str] = mapped_column(ForeignKey("samples.id"))
+    roi_id: Mapped[int] = mapped_column(ForeignKey("rois.id"))
+    label: Mapped[int] = mapped_column(Integer)
+    isotopologue_label: Mapped[int] = mapped_column(Integer)
+    isotopologue_index: Mapped[int] = mapped_column(Integer)
+    charge: Mapped[int] = mapped_column(Integer)
+    roi: Mapped["RoiModel"] = relationship(back_populates="annotations")
+    feature: Mapped["FeatureModel"] = relationship(back_populates="annotation")
+
+    def to_annotation(self) -> Annotation:
+        return Annotation(
+            label=self.label,
+            isotopologue_label=self.isotopologue_label,
+            isotopologue_index=self.isotopologue_index,
+            charge=self.charge,
+        )
+
+
+def _create_descriptor_table(feature_type: Type[Feature], metadata: MetaData):
+    table_name = "descriptors"
+
+    columns = [
+        Column("id", Integer, ForeignKey("features.id"), primary_key=True, autoincrement=True),
+        Column("sample_id", String, ForeignKey("samples.id")),
+        Column("roi_id", Integer, ForeignKey("rois.id")),
+        Column("label", Integer, ForeignKey("annotations.label")),
+    ]
+
+    descriptors = [Column(x, Float) for x in feature_type.descriptor_names()]
+    columns.extend(descriptors)
+    attrs = {"__tablename__": table_name, "__table__": Table(table_name, metadata, *columns)}
+
+    return type("DescriptorModel", (Base,), attrs)
+
+
 class AssayData:
 
-    Base = declarative_base()
-
-    class SampleModel(Base):
-        __tablename__ = "samples"
-        id: Mapped[str] = mapped_column(String, primary_key=True)
-        path: Mapped[str] = mapped_column(String, nullable=False)
-        ms_level: Mapped[int] = mapped_column(Integer)
-        start: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
-        end: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
-        group: Mapped[str] = mapped_column(String, nullable=True)
-
-    class RoiModel(Base):
-        __tablename__ = "rois"
-        sample_id: Mapped[str] = mapped_column(
-            String, ForeignKey("samples.id"), primary_key=True
-        )
-        roi_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-        data: Mapped[str] = mapped_column(String)
+    DescriptorModel: Type
 
     def __init__(self, name: Union[Path, str], roi: Type[Roi], feature: Type[Feature]):
 
@@ -61,15 +137,24 @@ class AssayData:
         self.feature = feature
 
         # Create a database engine and session factory
-        db_address = f"sqlite:///{name}.db"
+        if name:
+            db_address = f"sqlite:///{name}.db"
+        else:
+            db_address = "sqlite:///:memory:"
         self.engine = create_engine(db_address)
         self.SessionFactory = sessionmaker(bind=self.engine)
+        self._create_descriptor_model(feature)
 
         # Create the table in the database
-        self.Base.metadata.create_all(self.engine)
+        Base.metadata.create_all(self.engine)
+
+    @classmethod
+    def _create_descriptor_model(cls, feature_type: Type):
+        if not hasattr(cls, "DescriptorModel"):
+            cls.DescriptorModel = _create_descriptor_table(feature_type, Base.metadata)
 
     def add_samples(self, samples: list[Sample]):
-        samples = [self.SampleModel(**x.to_dict()) for x in samples]
+        samples = [SampleModel(**x.to_dict()) for x in samples]
         with self.SessionFactory() as session:
             try:
                 session.add_all(samples)
@@ -78,30 +163,63 @@ class AssayData:
                 msg = "Trying to insert a sample with an existing ID"
                 raise ValueError(msg)
 
-    def get_samples(self) -> list[Sample]:
+    def flag_processed(self, samples: list[Sample], step: str):
+        samples = [ProcessedSampleModel(sample_id=x.id, step=step) for x in samples]
         with self.SessionFactory() as session:
-            stmt = select(self.SampleModel)
+            try:
+                session.add_all(samples)
+                session.commit()
+            except IntegrityError:
+                msg = "Trying to insert a sample with an existing ID"
+                raise ValueError(msg)
+
+    def get_samples(self, step: Optional[str] = None) -> list[Sample]:
+        with self.SessionFactory() as session:
+            if step is None:
+                stmt = select(SampleModel)
+            else:
+                stmt = (
+                    select(SampleModel)
+                    .join(ProcessedSampleModel)
+                    .where(ProcessedSampleModel.step == step)
+                )
             result = session.execute(stmt)
             samples = list()
             for row in result:
                 sm = row.SampleModel
                 sample = Sample(Path(sm.path), sm.id, sm.ms_level, sm.start, sm.end, sm.group)
                 samples.append(sample)
-            return samples
+        return samples
 
     def delete_samples(self, sample_ids: list[str]) -> None:
         with self.SessionFactory() as session:
-            stmt = delete(self.SampleModel).where(self.SampleModel.id.in_(sample_ids))
+            stmt = delete(SampleModel).where(SampleModel.id.in_(sample_ids))
             session.execute(stmt)
             session.commit()
+
+    def set_processing_parameters(self, step: str, parameters: dict):
+        with self.SessionFactory() as session:
+            param_str = json.dumps(parameters)
+            params_model = ProcessedSampleModel(step=step, parameters=param_str)
+            session.add(params_model)
+            session.commit()
+
+    def get_processing_parameters(self, step: str) -> dict:
+        with self.SessionFactory() as session:
+            stmt = select(ProcessParameterModel).where(ProcessParameterModel.step == step)
+            results = session.execute(stmt)
+            param_model = results.first()
+            if param_model is None:
+                params = dict()
+            else:
+                params = json.loads(param_model.ProcessParameterModel.parameters)
+        return params
 
     def add_roi_list(self, roi_list: Sequence[Roi], sample: Sample):
 
         roi_model_list = list()
         for roi in roi_list:
-            roi_model = AssayData.RoiModel(
-                sample_id=sample.id, roi_id=roi.index, data=roi.to_string()
-            )
+            roi_model = RoiModel(sample_id=sample.id, data=roi.to_string())
             roi_model_list.append(roi_model)
         with self.SessionFactory() as session:
             try:
@@ -113,17 +231,114 @@ class AssayData:
 
     def get_roi_list(self, sample: Sample) -> list[Roi]:
         with self.SessionFactory() as session:
-            stmt = select(self.RoiModel).where(self.RoiModel.sample_id == sample.id)
+            stmt = select(RoiModel).where(RoiModel.sample_id == sample.id)
             result = session.execute(stmt)
             roi_list = list()
             for row in result:
                 rm = row.RoiModel
                 roi = self.roi.from_string(rm.data)
+                roi.id = rm.id
                 roi_list.append(roi)
-            return roi_list
-    
+        return roi_list
+
     def delete_roi_list(self, sample: Sample):
         with self.SessionFactory() as session:
-            stmt = delete(self.RoiModel).where(self.RoiModel.sample_id == sample.id)
+            stmt = delete(RoiModel).where(RoiModel.sample_id == sample.id)
             session.execute(stmt)
             session.commit()
+
+    def add_features(self, roi_list: Sequence[Roi], sample: Sample):
+        feature_model_list = list()
+        annotation_model_list = list()
+        descriptor_model_list = list()
+        for roi in roi_list:
+            if roi.features is not None:
+                for ft in roi.features:
+                    ft_model = FeatureModel(
+                        sample_id=sample.id, roi_id=roi.id, data=ft.to_str()
+                    )
+                    ann = ft.annotation
+                    annotation_model = AnnotationModel(
+                        sample_id=sample.id,
+                        roi_id=roi.id,
+                        label=ann.label,
+                        charge=ann.charge,
+                        isotopologue_label=ann.isotopologue_label,
+                        isotopologue_index=ann.isotopologue_index,
+                    )
+
+                    descriptor_model = self.DescriptorModel(
+                        sample_id=sample.id, roi_id=roi.id, label=ann.label, **ft.describe()
+                    )
+
+                    feature_model_list.append(ft_model)
+                    annotation_model_list.append(annotation_model)
+                    descriptor_model_list.append(descriptor_model)
+        with self.SessionFactory() as session:
+            try:
+                session.add_all(feature_model_list)
+                session.add_all(annotation_model_list)
+                session.add_all(descriptor_model_list)
+                session.commit()
+            except IntegrityError:
+                msg = "Trying to insert a sample with an existing ID"
+                raise ValueError(msg)
+
+    def get_features(
+        self,
+        sample: Optional[Sample] = None,
+        label: Optional[int] = None,
+        groups: Optional[list[str]] = None,
+    ) -> list[Feature]:
+        with self.SessionFactory() as session:
+
+            if sample is None:
+                if label is None:
+                    msg = "`sample` or `label` must be specified to select features"
+                    raise (ValueError(msg))
+                cond = AnnotationModel.label == label
+                if groups is not None:
+                    cond = cond and (SampleModel.group.in_(groups))
+            else:
+                cond = RoiModel.sample_id == sample.id
+
+            stmt = select(RoiModel).where(cond)
+            result = session.execute(stmt)
+            feature_list = list()
+            for row in result:
+                roi_model = row.RoiModel
+                roi = self.roi.from_string(roi_model.data)
+                for ft_model, ann_model in zip(roi_model.features, roi_model.annotations):
+                    ann = ann_model.to_annotation()
+                    ft = self.feature.from_str(ft_model.data, roi, ann)
+                    feature_list.append(ft)
+        return feature_list
+
+    def get_descriptors(
+        self, sample: Optional[Sample] = None, descriptors: Optional[list[str]] = None
+    ):
+        if descriptors is None:
+            descriptors = self.feature.descriptor_names()
+        else:
+            for d in descriptors:
+                valid_descriptors = self.feature.descriptor_names()
+                if d not in valid_descriptors:
+                    valid_str = ", ".join(valid_descriptors)
+                    msg = f"{d} is not a valid descriptor. Valid descriptors are: {valid_str}."
+                    raise ValueError(msg)
+
+        descriptors.extend(["label", "sample_id", "id"])
+        descriptor_list = {x: list() for x in descriptors}
+
+        with self.SessionFactory() as session:
+            if sample is None:
+                stmt = select(self.DescriptorModel)
+            else:
+                stmt = select(self.DescriptorModel).where(
+                    self.DescriptorModel.sample_id == sample.id
+                )
+            result = session.execute(stmt)
+            for row in result:
+                for d in descriptors:
+                    descriptor_list[d].append(row.DescriptorModel.__dict__[d])
+        return descriptor_list
--- tests/unit/assay/test_assay_data.py
+++ tests/unit/assay/test_assay_data.py
@@ -3,6 +3,7 @@ from tidyms.assay.assay_data import AssayData, Sample
 from tidyms.lcms import LCTrace, Peak
 from pathlib import Path
 import pytest
+from typing import cast
 
 
 def create_dummy_sample(path: Path, suffix: int) -> Sample:
@@ -11,28 +12,35 @@ def create_dummy_sample(path: Path, suffix: int) -> Sample:
     return sample
 
 
-def create_dummy_roi(index: int):
+def create_dummy_lc_trace():
     time = np.linspace(0, 20, 50)
     spint = np.abs(np.random.normal(size=time.size))
     mz = spint
     scans = np.arange(time.size)
-    return LCTrace(time, spint, mz, scans, index=index)
+    noise = np.zeros_like(mz)
+    baseline = np.zeros_like(mz)
+    return LCTrace(time, spint, mz, scans, noise=noise, baseline=baseline)
 
 
-def test_create_AssayData(tmp_path: Path):
-    path = tmp_path / "test-assay"
-    AssayData(path, LCTrace, Peak)
+def add_dummy_peaks(lc_trace: LCTrace, n: int):
+    features = list()
+    for k in range(n):
+        ft = Peak(0, 1, 2, lc_trace)
+        features.append(ft)
+    lc_trace.features = features
 
 
-def test_AssayData_add_samples_add_empty_list(tmp_path: Path):
-    path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+def test_create_AssayData():
+    AssayData("", LCTrace, Peak)
+
+
+def test_AssayData_add_samples_add_empty_list():
+    assay_data = AssayData("", LCTrace, Peak)
     assay_data.add_samples([])
 
 
-def test_AssayData_get_samples_from_empty_db(tmp_path: Path):
-    path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+def test_AssayData_get_samples_from_empty_db():
+    assay_data = AssayData("", LCTrace, Peak)
     assay_data.add_samples([])
     sample_list = assay_data.get_samples()
     assert len(sample_list) == 0
@@ -40,14 +48,14 @@ def test_AssayData_get_samples_from_empty_db(tmp_path: Path):
 
 def test_AssayData_add_samples_add_single_sample(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = [create_dummy_sample(path, 1)]
     assay_data.add_samples(sample)
 
 
 def test_AssayData_get_samples_single_sample(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     expected_sample = [create_dummy_sample(path, 1)]
     assay_data.add_samples(expected_sample)
     test_sample = assay_data.get_samples()
@@ -56,14 +64,14 @@ def test_AssayData_get_samples_single_sample(tmp_path: Path):
 
 def test_AssayData_add_samples_multiple_samples(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample_list = [create_dummy_sample(path, x) for x in range(10, 20)]
     assay_data.add_samples(sample_list)
 
 
 def test_AssayData_get_samples_multiple_samples(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     expected_sample_list = [create_dummy_sample(path, x) for x in range(10, 20)]
     assay_data.add_samples(expected_sample_list)
     test_sample_list = assay_data.get_samples()
@@ -72,7 +80,7 @@ def test_AssayData_get_samples_multiple_samples(tmp_path: Path):
 
 def test_AssayData_add_samples_add_existing_sample(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     expected_sample = [create_dummy_sample(path, 1)]
     assay_data.add_samples(expected_sample)
     with pytest.raises(ValueError):
@@ -81,7 +89,7 @@ def test_AssayData_add_samples_add_existing_sample(tmp_path: Path):
 
 def test_AssayData_delete_samples(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample_list = [create_dummy_sample(path, x) for x in range(10, 20)]
     assay_data.add_samples(sample_list)
 
@@ -99,36 +107,38 @@ def test_AssayData_delete_samples(tmp_path: Path):
 
 def test_AssayData_add_roi_list(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     assay_data.add_samples([sample])
-    roi_list = [create_dummy_roi(x) for x in range(20)]
+    roi_list = [create_dummy_lc_trace() for x in range(20)]
     assay_data.add_roi_list(roi_list, sample)
 
 
 def test_AssayData_get_roi_list(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     assay_data.add_samples([sample])
-    expected_roi_list = [create_dummy_roi(x) for x in range(20)]
+    expected_roi_list = [create_dummy_lc_trace() for _ in range(20)]
     assay_data.add_roi_list(expected_roi_list, sample)
     test_roi_list = assay_data.get_roi_list(sample)
     assert expected_roi_list == test_roi_list
+    for k, roi in enumerate(test_roi_list):
+        assert (k + 1) == roi.id  # int autoincrement in DB starts at 1
 
 
-def test_AssayData_add_empty_list(tmp_path: Path):
+def test_AssayData_add_roi_list_empty_list(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     assay_data.add_samples([sample])
     roi_list = list()
     assay_data.add_roi_list(roi_list, sample)
 
 
-def test_AssayData_get_empty_list(tmp_path: Path):
+def test_AssayData_get_roi_list_empty_list(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     assay_data.add_samples([sample])
     expected_roi_list = list()
@@ -139,10 +149,10 @@ def test_AssayData_get_empty_list(tmp_path: Path):
 
 def test_AssayData_delete_roi_list(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     assay_data.add_samples([sample])
-    roi_list = [create_dummy_roi(x) for x in range(20)]
+    roi_list = [create_dummy_lc_trace() for _ in range(20)]
     assay_data.add_roi_list(roi_list, sample)
     assay_data.delete_roi_list(sample)
     test_roi_list = assay_data.get_roi_list(sample)
@@ -152,12 +162,165 @@ def test_AssayData_delete_roi_list(tmp_path: Path):
 
 def test_AssayData_delete_roi_list_no_delete(tmp_path: Path):
     path = tmp_path / "test-assay"
-    assay_data = AssayData(path, LCTrace, Peak)
+    assay_data = AssayData("", LCTrace, Peak)
     sample = create_dummy_sample(path, 1)
     sample2 = create_dummy_sample(path, 2)
     assay_data.add_samples([sample])
-    expected_roi_list = [create_dummy_roi(x) for x in range(20)]
+    expected_roi_list = [create_dummy_lc_trace() for _ in range(20)]
     assay_data.add_roi_list(expected_roi_list, sample)
     assay_data.delete_roi_list(sample2)
     test_roi_list = assay_data.get_roi_list(sample)
     assert expected_roi_list == test_roi_list
+
+
+def test_AssayData_add_features(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample = create_dummy_sample(tmp_path, 1)
+    assay_data.add_samples([sample])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(20)]
+    assay_data.add_roi_list(roi_list, sample)
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample)
+
+
+def test_AssayData_get_features_by_sample(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample = create_dummy_sample(tmp_path, 1)
+    assay_data.add_samples([sample])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(20)]
+    assay_data.add_roi_list(roi_list, sample)
+
+    # roi_list with roi.id values
+    roi_list = cast(list[LCTrace], assay_data.get_roi_list(sample))
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample)
+
+    expected_feature_list: list[Peak] = list()
+    for roi in roi_list:
+        expected_feature_list.extend(roi.features)
+
+    test_feature_list = cast(list[Peak], assay_data.get_features(sample))
+
+    assert len(test_feature_list) == len(expected_feature_list)
+    for eft, tft in zip(expected_feature_list, test_feature_list):
+        assert eft.start == tft.start
+        assert eft.apex == tft.apex
+        assert eft.end == tft.end
+        assert eft.roi == tft.roi
+
+
+def test_AssayData_add_features_no_features(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample = create_dummy_sample(tmp_path, 1)
+    assay_data.add_samples([sample])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(20)]
+    assay_data.add_roi_list(roi_list, sample)
+
+    # rois do not have features
+    assay_data.add_features(roi_list, sample)
+
+
+def test_AssayData_get_features_by_sample_no_features(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample = create_dummy_sample(tmp_path, 1)
+    assay_data.add_samples([sample])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(20)]
+    assay_data.add_roi_list(roi_list, sample)
+
+    # rois do not have features
+    assay_data.add_features(roi_list, sample)
+
+    test_features = assay_data.get_features(sample)
+    assert len(test_features) == 0
+
+
+def test_AssayData_get_descriptors_one_samples(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample1 = create_dummy_sample(tmp_path, 1)
+    sample2 = create_dummy_sample(tmp_path, 2)
+    assay_data.add_samples([sample1, sample2])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(10)]
+    assay_data.add_roi_list(roi_list, sample1)
+    assay_data.add_roi_list(roi_list, sample2)
+
+    # roi_list with roi.id values
+    roi_list = cast(list[LCTrace], assay_data.get_roi_list(sample1))
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+
+    assay_data.add_features(roi_list, sample1)
+
+    # roi_list with roi.id values
+    roi_list = cast(list[LCTrace], assay_data.get_roi_list(sample2))
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample2)
+
+    descriptors = assay_data.get_descriptors(sample1)
+    for v in descriptors.values():
+        assert len(v) == 20
+
+
+def test_AssayData_get_descriptors_all_samples(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add samples
+    sample1 = create_dummy_sample(tmp_path, 1)
+    sample2 = create_dummy_sample(tmp_path, 2)
+    assay_data.add_samples([sample1, sample2])
+
+    # add roi
+    roi_list = [create_dummy_lc_trace() for _ in range(10)]
+    assay_data.add_roi_list(roi_list, sample1)
+    assay_data.add_roi_list(roi_list, sample2)
+
+    # roi_list with roi.id values
+    roi_list = cast(list[LCTrace], assay_data.get_roi_list(sample1))
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+
+    assay_data.add_features(roi_list, sample1)
+
+    # roi_list with roi.id values
+    roi_list = cast(list[LCTrace], assay_data.get_roi_list(sample2))
+
+    # add features
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample2)
+
+    descriptors = assay_data.get_descriptors()
+    for v in descriptors.values():
+        assert len(v) == 40
