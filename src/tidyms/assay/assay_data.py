9f4fcd791b87cb67728fdb203e63036636ba7bd7
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-04-20 09:36:05 -0300

    refactor: WIP: AssayData.

--- src/tidyms/assay/assay_data.py
+++ src/tidyms/assay/assay_data.py
@@ -3,12 +3,19 @@ from typing import Optional, Sequence, Type, Union
 from pathlib import Path
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table, MetaData
 from sqlalchemy import create_engine, delete, select
-from sqlalchemy.orm import declarative_base, sessionmaker, Mapped, mapped_column, relationship
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker,
+    Mapped,
+    mapped_column,
+    relationship,
+)
 from sqlalchemy.exc import IntegrityError
 from ..lcms import Annotation, Feature, Roi
+from .. import _constants as c
 import json
 
-# TODO: get sample by processing stage
+
 # TODO: Idea: Assays are created using create_assay and load_assay functions.
 
 
@@ -44,9 +51,6 @@ class Sample:
     def __post_init__(self):
         if isinstance(self.path, str):
             self.path = Path(self.path)
-
-        if self.id is None:
-            self.id = self.path.stem
 
     def to_dict(self) -> dict:
         d = asdict(self)
@@ -84,6 +88,11 @@ class SampleModel(Base):
     rois: Mapped[list["RoiModel"]] = relationship(back_populates="samples")
     features: Mapped[list["FeatureModel"]] = relationship(back_populates="samples")
 
+    def to_sample(self) -> Sample:
+        return Sample(
+            str(self.path), self.id, self.ms_level, self.start, self.end, self.group
+        )
+
 
 class ProcessedSampleModel(Base):
     """
@@ -114,6 +123,11 @@ class RoiModel(Base):
     annotations: Mapped[list["AnnotationModel"]] = relationship(
         back_populates="roi", lazy="immediate"
     )
+
+    def to_roi(self, roi_type: Type[Roi]) -> Roi:
+        roi = roi_type.from_string(self.data)
+        roi.id = self.id
+        return roi
 
 
 class FeatureModel(Base):
@@ -181,7 +195,13 @@ def _create_descriptor_table(feature_type: Type[Feature], metadata: MetaData):
     table_name = "descriptors"
 
     columns = [
-        Column("id", Integer, ForeignKey("features.id"), primary_key=True, autoincrement=True),
+        Column(
+            "id",
+            Integer,
+            ForeignKey("features.id"),
+            primary_key=True,
+            autoincrement=True,
+        ),
         Column("sample_id", String, ForeignKey("samples.id")),
         Column("roi_id", Integer, ForeignKey("rois.id")),
         Column("label", Integer, ForeignKey("annotations.label")),
@@ -189,21 +209,23 @@ def _create_descriptor_table(feature_type: Type[Feature], metadata: MetaData):
 
     descriptors = [Column(x, Float) for x in feature_type.descriptor_names()]
     columns.extend(descriptors)
-    attrs = {"__tablename__": table_name, "__table__": Table(table_name, metadata, *columns)}
+    attrs = {
+        "__tablename__": table_name,
+        "__table__": Table(table_name, metadata, *columns),
+    }
 
     return type("DescriptorModel", (Base,), attrs)
 
 
 class AssayData:
     """
-    Manages Sample, Roi and Feature persistence in an Assay.
+    Manages Sample, Roi and Feature persistence in an Assay using a SQLite DataBase.
 
     """
 
     DescriptorModel: Type
 
     def __init__(self, name: Union[Path, str], roi: Type[Roi], feature: Type[Feature]):
-
         self.roi = roi
         self.feature = feature
 
@@ -221,10 +243,32 @@ class AssayData:
 
     @classmethod
     def _create_descriptor_model(cls, feature_type: Type):
+        """
+        Create the DescriptorModel table using descriptors available in the Feature class.
+
+        Parameters
+        ----------
+        feature_type : Feature
+            Feature type stored in the AssayData.
+
+        """
         if not hasattr(cls, "DescriptorModel"):
             cls.DescriptorModel = _create_descriptor_table(feature_type, Base.metadata)
 
     def add_samples(self, samples: list[Sample]):
+        """
+        Add samples into the DB.
+
+        Parameters
+        ----------
+        samples : list[Sample]
+
+        Raises
+        ------
+        ValueError
+            If a sample with the same id already exists in the DB.
+
+        """
         samples = [SampleModel(**x.to_dict()) for x in samples]
         with self.SessionFactory() as session:
             try:
@@ -234,17 +278,51 @@ class AssayData:
                 msg = "Trying to insert a sample with an existing ID"
                 raise ValueError(msg)
 
-    def flag_processed(self, samples: list[Sample], step: str):
+    def flag_processed_samples(self, samples: list[Sample], step: str):
+        """
+        Flags samples as processed in a preprocessing step.
+
+        Parameters
+        ----------
+        samples : list[Sample]
+        step : str
+            Name of a preprocessing step.
+
+        Raises
+        ------
+        ValueError
+            If the preprocessing step name is not a valid name. Valid names
+            are defined by `PREPROCESSING_STEPS` in the `_constants` module.
+
+        """
+        _check_preprocessing_step(step)
         samples = [ProcessedSampleModel(sample_id=x.id, step=step) for x in samples]
         with self.SessionFactory() as session:
-            try:
-                session.add_all(samples)
-                session.commit()
-            except IntegrityError:
-                msg = "Trying to insert a sample with an existing ID"
-                raise ValueError(msg)
+            session.add_all(samples)
+            session.commit()
 
     def get_samples(self, step: Optional[str] = None) -> list[Sample]:
+        """
+        Load the samples stored in the DB.
+
+        Parameters
+        ----------
+        step : str or None, default=None
+            Name of a preprocessing step. If provided, returns all samples processed
+            by that processing step. If ``None``, return all samples in the DB.
+
+        Returns
+        -------
+        list[Sample]
+
+        Raises
+        ------
+        ValueError
+            If the preprocessing step name is not a valid name. Valid names
+            are defined by `PREPROCESSING_STEPS` in the `_constants` module.
+
+        """
+        _check_preprocessing_step(step)
         with self.SessionFactory() as session:
             if step is None:
                 stmt = select(SampleModel)
@@ -257,27 +335,75 @@ class AssayData:
             result = session.execute(stmt)
             samples = list()
             for row in result:
-                sm = row.SampleModel
-                sample = Sample(Path(sm.path), sm.id, sm.ms_level, sm.start, sm.end, sm.group)
-                samples.append(sample)
+                samples.append(row.SampleModel.to_sample())
         return samples
 
     def delete_samples(self, sample_ids: list[str]) -> None:
+        """
+        Delete samples stored in the DB.
+
+        Parameters
+        ----------
+        sample_ids : list[str]
+            Unique IDs of samples in the DB.
+
+        """
         with self.SessionFactory() as session:
             stmt = delete(SampleModel).where(SampleModel.id.in_(sample_ids))
             session.execute(stmt)
             session.commit()
 
     def set_processing_parameters(self, step: str, parameters: dict):
+        """
+        Stores preprocessing parameters of a step in the DB.
+
+        Parameters
+        ----------
+        step : str
+            Preprocessing step name.
+        parameters : dict
+            Parameters used in the preprocessing step.
+
+        Raises
+        ------
+        ValueError
+            If the preprocessing step name is not a valid name. Valid names
+            are defined by `PREPROCESSING_STEPS` in the `_constants` module.
+
+        """
+        _check_preprocessing_step(step)
         with self.SessionFactory() as session:
             param_str = json.dumps(parameters)
-            params_model = ProcessedSampleModel(step=step, parameters=param_str)
+            params_model = ProcessParameterModel(step=step, parameters=param_str)
             session.add(params_model)
             session.commit()
 
     def get_processing_parameters(self, step: str) -> dict:
+        """
+        Retrieves preprocessing parameters of a step from the DB.
+
+        Parameters
+        ----------
+        step : str
+            Preprocessing step name.
+
+        Returns
+        -------
+        parameters : dict
+            Parameters used in the preprocessing step.
+
+        Raises
+        ------
+        ValueError
+            If the preprocessing step name is not a valid name. Valid names
+            are defined by `PREPROCESSING_STEPS` in the `_constants` module.
+
+        """
+        _check_preprocessing_step(step)
         with self.SessionFactory() as session:
-            stmt = select(ProcessParameterModel).where(ProcessParameterModel.step == step)
+            stmt = select(ProcessParameterModel).where(
+                ProcessParameterModel.step == step
+            )
             results = session.execute(stmt)
             param_model = results.first()
             if param_model is None:
@@ -287,28 +413,42 @@ class AssayData:
         return params
 
     def add_roi_list(self, roi_list: Sequence[Roi], sample: Sample):
+        """
+        Stores a list of ROI extracted from a sample in the DB.
 
+        Parameters
+        ----------
+        roi_list : Sequence[Roi]
+        sample : Sample
+
+        """
         roi_model_list = list()
         for roi in roi_list:
             roi_model = RoiModel(sample_id=sample.id, data=roi.to_string())
             roi_model_list.append(roi_model)
         with self.SessionFactory() as session:
-            try:
-                session.add_all(roi_model_list)
-                session.commit()
-            except IntegrityError:
-                msg = "Trying to insert a sample with an existing ID"
-                raise ValueError(msg)
+            session.add_all(roi_model_list)
+            session.commit()
 
     def get_roi_list(self, sample: Sample) -> list[Roi]:
+        """
+        Retrieves a list of ROI detected in a sample.
+
+        Parameters
+        ----------
+        sample : Sample
+
+        Returns
+        -------
+        list[Roi]
+
+        """
         with self.SessionFactory() as session:
             stmt = select(RoiModel).where(RoiModel.sample_id == sample.id)
             result = session.execute(stmt)
             roi_list = list()
             for row in result:
-                rm = row.RoiModel
-                roi = self.roi.from_string(rm.data)
-                roi.id = rm.id
+                roi = row.RoiModel.to_roi(self.roi)
                 roi_list.append(roi)
         return roi_list
 
@@ -339,21 +479,20 @@ class AssayData:
                     )
 
                     descriptor_model = self.DescriptorModel(
-                        sample_id=sample.id, roi_id=roi.id, label=ann.label, **ft.describe()
+                        sample_id=sample.id,
+                        roi_id=roi.id,
+                        label=ann.label,
+                        **ft.describe(),
                     )
 
                     feature_model_list.append(ft_model)
                     annotation_model_list.append(annotation_model)
                     descriptor_model_list.append(descriptor_model)
         with self.SessionFactory() as session:
-            try:
-                session.add_all(feature_model_list)
-                session.add_all(annotation_model_list)
-                session.add_all(descriptor_model_list)
-                session.commit()
-            except IntegrityError:
-                msg = "Trying to insert a sample with an existing ID"
-                raise ValueError(msg)
+            session.add_all(feature_model_list)
+            session.add_all(annotation_model_list)
+            session.add_all(descriptor_model_list)
+            session.commit()
 
     def get_features(
         self,
@@ -362,7 +501,6 @@ class AssayData:
         groups: Optional[list[str]] = None,
     ) -> list[Feature]:
         with self.SessionFactory() as session:
-
             if sample is None:
                 if label is None:
                     msg = "`sample` or `label` must be specified to select features"
@@ -370,16 +508,17 @@ class AssayData:
                 cond = AnnotationModel.label == label
                 if groups is not None:
                     cond = cond and (SampleModel.group.in_(groups))
+                stmt = select(RoiModel).join(FeatureModel).where(cond)
             else:
                 cond = RoiModel.sample_id == sample.id
+                stmt = select(RoiModel).where(cond)
 
-            stmt = select(RoiModel).where(cond)
             result = session.execute(stmt)
             feature_list = list()
             for row in result:
-                roi_model = row.RoiModel
-                roi = self.roi.from_string(roi_model.data)
-                for ft_model, ann_model in zip(roi_model.features, roi_model.annotations):
+                rm = row.RoiModel
+                roi = rm.to_roi(self.roi)
+                for ft_model, ann_model in zip(rm.features, rm.annotations):
                     ann = ann_model.to_annotation()
                     ft = self.feature.from_str(ft_model.data, roi, ann)
                     feature_list.append(ft)
@@ -413,3 +552,12 @@ class AssayData:
                 for d in descriptors:
                     descriptor_list[d].append(row.DescriptorModel.__dict__[d])
         return descriptor_list
+
+
+def _check_preprocessing_step(step: Optional[str]):
+    if (step is not None) and (step not in c.PREPROCESSING_STEPS):
+        valid_steps = ", ".join(c.PREPROCESSING_STEPS)
+        msg = (
+            f"{step} is not a valid preprocessing step. valid values are {valid_steps}."
+        )
+        raise ValueError(msg)
--- tests/unit/assay/test_assay_data.py
+++ tests/unit/assay/test_assay_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tidyms.assay.assay_data import AssayData, Sample
-from tidyms.lcms import LCTrace, Peak
+from tidyms.lcms import LCTrace, Peak, Annotation
 from pathlib import Path
 import pytest
 from typing import cast
@@ -25,7 +25,8 @@ def create_dummy_lc_trace():
 def add_dummy_peaks(lc_trace: LCTrace, n: int):
     features = list()
     for k in range(n):
-        ft = Peak(0, 1, 2, lc_trace)
+        annotation = Annotation(label=k)
+        ft = Peak(0, 1, 2, lc_trace, annotation)
         features.append(ft)
     lc_trace.features = features
 
@@ -105,6 +106,72 @@ def test_AssayData_delete_samples(tmp_path: Path):
     assert test_sample_list == sample_list
 
 
+def test_AssayData_flag_samples(tmp_path: Path):
+    samples = [create_dummy_sample(tmp_path, x) for x in range(1, 11)]
+    assay_data = AssayData("", LCTrace, Peak)
+    assay_data.add_samples(samples)
+    step = "detect_features"
+    assay_data.flag_processed_samples(samples, step)
+
+
+def test_AssayData_flag_samples_invalid_step(tmp_path: Path):
+    samples = [create_dummy_sample(tmp_path, x) for x in range(1, 11)]
+    assay_data = AssayData("", LCTrace, Peak)
+    assay_data.add_samples(samples)
+    step = "invalid_preprocessing_step"
+    with pytest.raises(ValueError):
+        assay_data.flag_processed_samples(samples, step)
+
+
+def test_AssayData_get_samples_processing_stage(tmp_path: Path):
+    samples = [create_dummy_sample(tmp_path, x) for x in range(1, 11)]
+    assay_data = AssayData("", LCTrace, Peak)
+    assay_data.add_samples(samples)
+    step = "detect_features"
+    assay_data.flag_processed_samples(samples, step)
+
+    new_samples = [create_dummy_sample(tmp_path, x) for x in range(20, 30)]
+    assay_data.add_samples(new_samples)
+
+    test_samples = assay_data.get_samples(step=step)
+    all_samples = assay_data.get_samples()
+    assert samples == test_samples
+    assert all_samples != test_samples
+
+
+def test_AssayData_get_samples_invalid_step(tmp_path: Path):
+    samples = [create_dummy_sample(tmp_path, x) for x in range(1, 11)]
+    assay_data = AssayData("", LCTrace, Peak)
+    assay_data.add_samples(samples)
+    step = "invalid_preprocessing_step"
+    with pytest.raises(ValueError):
+        assay_data.get_samples(step)
+
+
+def test_AssayData_set_processing_parameters():
+    assay_data = AssayData("", LCTrace, Peak)
+    step = "detect_features"
+    parameters = {"a": 1, "b": 2.0, "c": "a"}
+    assay_data.set_processing_parameters(step, parameters)
+
+
+def test_AssayData_set_processing_parameters_invalid_step():
+    assay_data = AssayData("", LCTrace, Peak)
+    step = "invalid_preprocessing_step"
+    parameters = {"a": 1, "b": 2.0, "c": "a"}
+    with pytest.raises(ValueError):
+        assay_data.set_processing_parameters(step, parameters)
+
+
+def test_AssayData_get_processing_parameters_invalid_step():
+    assay_data = AssayData("", LCTrace, Peak)
+    step = "detect_features"
+    expected_parameters = {"a": 1, "b": 2.0, "c": "a"}
+    assay_data.set_processing_parameters(step, expected_parameters)
+    tests_parameters = assay_data.get_processing_parameters(step)
+    assert expected_parameters == tests_parameters
+
+
 def test_AssayData_add_roi_list(tmp_path: Path):
     path = tmp_path / "test-assay"
     assay_data = AssayData("", LCTrace, Peak)
@@ -123,8 +190,6 @@ def test_AssayData_get_roi_list(tmp_path: Path):
     assay_data.add_roi_list(expected_roi_list, sample)
     test_roi_list = assay_data.get_roi_list(sample)
     assert expected_roi_list == test_roi_list
-    for k, roi in enumerate(test_roi_list):
-        assert (k + 1) == roi.id  # int autoincrement in DB starts at 1
 
 
 def test_AssayData_add_roi_list_empty_list(tmp_path: Path):
@@ -221,6 +286,35 @@ def test_AssayData_get_features_by_sample(tmp_path: Path):
         assert eft.apex == tft.apex
         assert eft.end == tft.end
         assert eft.roi == tft.roi
+
+
+def test_AssayData_get_features_by_label(tmp_path: Path):
+    assay_data = AssayData("", LCTrace, Peak)
+
+    # add roi and features for sample 1
+    sample1 = create_dummy_sample(tmp_path, 1)
+    assay_data.add_samples([sample1])
+    roi_list = [create_dummy_lc_trace() for _ in range(0)]
+    assay_data.add_roi_list(roi_list, sample1)
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample1)
+
+    # add roi and features for sample 1
+    sample2 = create_dummy_sample(tmp_path, 2)
+    assay_data.add_samples([sample2])
+    roi_list = [create_dummy_lc_trace() for _ in range(0)]
+    assay_data.add_roi_list(roi_list, sample2)
+    for roi in roi_list:
+        add_dummy_peaks(roi, 2)
+    assay_data.add_features(roi_list, sample2)
+
+    test_label = 1
+    test_feature_list = cast(list[Peak], assay_data.get_features(label=test_label))
+
+    assert len(test_feature_list) == 2
+    for ft in test_feature_list:
+        assert ft.annotation.label == test_label
 
 
 def test_AssayData_add_features_no_features(tmp_path: Path):
