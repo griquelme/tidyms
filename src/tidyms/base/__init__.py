3deb56b4090ff3d38d1d29e883735083c2812739
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-05-22 16:48:25 -0300

    Add Assay to subpackage __init__.

--- src/tidyms/base/__init__.py
+++ src/tidyms/base/__init__.py
@@ -1,13 +1,15 @@
 from .legacy_assay import LegacyAssay
+from .assay import Assay
 from .assay_data import AssayData, Sample, SampleData
 from .assay_processor import ProcessingPipeline
 from .isotopologue_annotator import IsotopologueAnnotator
 from . import lcms_assay
 
 __all__ = [
-    "LegacyAssay",
+    "Assay",
     "AssayData",
     "IsotopologueAnnotator",
+    "LegacyAssay",
     "ProcessingPipeline",
     "Sample",
     "SampleData",
