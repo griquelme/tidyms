eec271e2cb0acfcd71f94f4f34de9a88929ceea0
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-06-04 11:21:32 -0300

    refactor: Add IsotopologueAnnotator to annotation package __init__.

--- src/tidyms/annotation/__init__.py
+++ src/tidyms/annotation/__init__.py
@@ -18,5 +18,11 @@ create_annotation_tools
 """
 
 from .annotation import annotate, create_annotation_table, create_annotation_tools
+from .isotopologue_annotator import IsotopologueAnnotator
 
-__all__ = ["annotate", "create_annotation_tools", "create_annotation_table"]
+__all__ = [
+    "annotate",
+    "create_annotation_tools",
+    "create_annotation_table",
+    "IsotopologueAnnotator",
+]
