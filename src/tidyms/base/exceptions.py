15f90a5c462b1373b1de858d2035c605cf65187e
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-01-21 16:28:33 -0300

    feat: Add exceptions module.

---
+++ src/tidyms/base/exceptions.py
@@ -0,0 +1,7 @@
+"""Exceptions raised by tidyms."""
+
+
+class IncompatibleSampleDataStatus(ValueError):
+    """Exception raised when the sample data is not compatible with the expected status."""
+
+    pass
