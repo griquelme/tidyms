d3a134e832942c14ac448504920d93705c3344f4
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-02-11 15:13:21 -0300

    feat: Add specific exceptions.

--- src/tidyms/core/exceptions.py
+++ src/tidyms/core/exceptions.py
@@ -1,11 +1,25 @@
 """Exceptions raised by tidyms."""
 
 
+class FeatureTypeNotRegistered(ValueError):
+    """"Exception raised when trying to fetch a non-registered Feature type."""
+
+
 class IncompatibleProcessorStatus(ValueError):
     """Exception raised when the sample data is not compatible with the expected status."""
 
-    pass
 
-
-class InvalidProcessingPipeline(ValueError):
+class InvalidPipelineConfiguration(ValueError):
     """Exception raised when a processing pipeline has an invalid configuration."""
+
+
+class ProcessorNotFound(ValueError):
+    """Exception raised when a processor is not found in a processing pipeline."""
+
+
+class ProcessorTypeNotRegistered(ValueError):
+    """"Exception raised when trying to fetch a non-registered Processor type."""
+
+
+class RoiTypeNotRegistered(ValueError):
+    """"Exception raised when trying to fetch a non-registered ROI type."""
